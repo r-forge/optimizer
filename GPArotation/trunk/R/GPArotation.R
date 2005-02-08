2be57a251deb628ea7bdc0bf0549734fa46b4fd7
cvs <unknown@noreply.com>
2005-02-08 04:05:45 +0000

    added term to vgQ.entropy to prevent log 0.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- GPArotation/trunk/R/GPArotation.R
+++ GPArotation/trunk/R/GPArotation.R
@@ -287,8 +287,8 @@ entropy <- function(L, Tmat=diag(ncol(L)), normalize=FALSE, eps=1e-5, maxit=1000
    }
 
 vgQ.entropy <- function(L){
-  list(Gq= -(L*log(L^2) + L),
-       f= -sum(L^2*log(L^2))/2, 
+  list(Gq= -(L*log(L^2 + (L^2==0)) + L),
+       f= -sum(L^2*log(L^2 + (L^2==0)))/2, 
        Method="Minimum entropy")
 }
 

f3e55aca87ebc874ef86d6b6ac341136507d190f
cvs <unknown@noreply.com>
2006-08-27 02:01:03 +0000

    working version of eiv in GPArotation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- GPArotation/trunk/R/eiv.R
+++ GPArotation/trunk/R/eiv.R
@@ -1,9 +1,12 @@
 eiv <- function(L,  identity=seq(ncol(L)), normalize=FALSE){
    A1 <- L[ identity, , drop=FALSE]
    Th <- solve(A1)
+   if(1e-14 < max(abs(diag(1, length(identity)) - A1 %*% Th)))
+      warning("Inverse is not well conditioned. Consider setting identity to select different rows.")
    B <- array(NA, dim(L))
-   B[identity, ] <- diag(1, nrow(identity))
+   B[identity, ] <- diag(1, length(identity))
    B[-identity,] <- L[-identity,, drop=FALSE] %*% Th
+   dimnames(B) <- list(dimnames(L)[[1]], paste("factor", seq(length(identity))))
    list(loadings=B, Th=Th, method="eiv", orthogonal=FALSE, convergence=TRUE,
         Phi= t(Th) %*% Th)
    }

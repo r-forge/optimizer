8c2805675442749a88a32dabaecbe6c5a22083d4
cvs <unknown@noreply.com>
2006-08-31 21:18:47 +0000

    working initial version of eiv.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@141 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- GPArotation/trunk/R/eiv.R
+++ GPArotation/trunk/R/eiv.R
@@ -1,13 +1,12 @@
-eiv <- function(L,  identity=seq(ncol(L)), normalize=FALSE){
+eiv <- function(L,  identity=seq(ncol(L))){
    A1 <- L[ identity, , drop=FALSE]
-   Th <- solve(A1)
-   if(1e-14 < max(abs(diag(1, length(identity)) - A1 %*% Th)))
+   g <- solve(A1)
+   if(1e-14 < max(abs(diag(1, length(identity)) - A1 %*% g)))
       warning("Inverse is not well conditioned. Consider setting identity to select different rows.")
    B <- array(NA, dim(L))
    B[identity, ] <- diag(1, length(identity))
-   B[-identity,] <- L[-identity,, drop=FALSE] %*% Th
+   B[-identity,] <- L[-identity,, drop=FALSE] %*% g
    dimnames(B) <- list(dimnames(L)[[1]], paste("factor", seq(length(identity))))
-   list(loadings=B, Th=Th, method="eiv", orthogonal=FALSE, convergence=TRUE,
-        Phi= t(Th) %*% Th)
+   list(loadings=B, Th=t(A1), method="eiv", orthogonal=FALSE, convergence=TRUE,
+        Phi= tcrossprod(A1))
    }
-

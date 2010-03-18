a4656014cd5384e790800b086ea4891c701ff950
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2010-03-18 21:10:31 +0000

    Allocate working storage in minqa.cpp; use Rcpp::as<T> extensively so a length of 1 is checked; switch returned object class and method of generation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@303 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/minqa/src/minqa.cpp
+++ pkg/minqa/src/minqa.cpp
@@ -1,8 +1,9 @@
 #include <Rcpp.h>
 #include <R_ext/RS.h>
+#include <R_ext/Rdynload.h>
 
 /// Wrapper for the objective function.  It is initialized to R's "c" function
-Rcpp::Function cf("c");
+static Rcpp::Function cf("c");
 
 /// Apply is.finite to elements in a transformation
 static inline double check_finite(double x) {
@@ -12,8 +13,7 @@ static inline double check_finite(double x) {
 
 /// Fortran callable objective function evaluation.
 extern "C"
-double F77_NAME(calfun)(const int *n, const double x[], const int *ip)
-{
+double F77_NAME(calfun)(const int *n, const double x[], const int *ip) {
     Rcpp::Environment rho(cf.environment());
     Rcpp::IntegerVector cc(rho.get(".feval."));
     Rcpp::NumericVector pp(rho.get(".par."));
@@ -32,51 +32,51 @@ double F77_NAME(calfun)(const int *n, const double x[], const int *ip)
     return f;
 }
 
+/// Names of returned values
+static Rcpp::Argument parnm("par"), fval("fval"), feval("feval");
+
+/// Return the number of function evaluations as an SEXP
+static SEXP fevalf() {
+    Rcpp::Environment rho(cf.environment());
+    return rho.get(".feval.");
+}
+
+/// Construct the classed list to return
+static SEXP rval(Rcpp::NumericVector par, std::string cnm) {
+    Rcpp::StringVector cl(2);
+    int ip = 0, n = par.size();
+
+    Rcpp::List rr =
+	make_list(parnm = par, fval = F77_NAME(calfun)(&n, par.begin(), &ip),
+		  feval = fevalf());
+    cl[0] = cnm;
+    cl[1] = "minqa";
+    rr.attr("class") = cl;
+    return wrap(rr);
+}
+
 /// Declaration of Powell's bobyqa
 extern "C" 
 void F77_NAME(bobyqa)(const int *n, const int *npt, double X[],
 		      const double xl[], const double xu[],
 		      const double *rhobeg, const double *rhoend,
-		      const int *iprint, const int *maxfun, double w[],
-		      double fval[]);
+		      const int *iprint, const int *maxfun, double w[]);
 
 /// Interface for bobyqa
-RcppExport SEXP bobyqa_cpp(SEXP ppar, SEXP pxl, SEXP pxu, 
-			   SEXP ctrl, SEXP fn, SEXP work)
-{
-    Rcpp::Environment cc(ctrl); 
-    Rcpp::NumericVector par(ppar), rb(cc.get("rhobeg")),
-	re(cc.get("rhoend")), xl(pxl), xu(pxu), wrk(work);
-    Rcpp::IntegerVector npt(cc.get("npt")), mxf(cc.get("maxfun")),
-	ip(cc.get("iprint"));
+RcppExport SEXP bobyqa_cpp(SEXP ppar, SEXP pxl, SEXP pxu, SEXP pcc, SEXP fn) {
+    Rcpp::Environment cc(pcc);
+    Rcpp::NumericVector par(ppar), xl(pxl), xu(pxu);
     cf = Rcpp::Function(fn);	// install the objective function
-    Rcpp::Environment rho(cf.environment());
-
-    // Ensure that all the control settings contain at least one element
-    if (!(rb.size() && re.size() && npt.size() && mxf.size() && ip.size()))
-	Rf_error("Incomplete control parameter list");
-
-    int n = par.size();
-    if (xl.size() != n || xu.size() != n) // check sizes (lengths)
-	Rf_error("sizes of par, xl and xu don't match");
-
-    // Allocate memory for function values used for approximation.  Is
-    // this used at all? It looks like it was added after the fact.
-    Rcpp::NumericVector fval(*(npt.begin())); 
-
-    F77_NAME(bobyqa)(&n, npt.begin(), par.begin(), xl.begin(),
-		     xu.begin(), rb.begin(), re.begin(), ip.begin(),
-		     mxf.begin(), wrk.begin(), fval.begin());
-
-    Rcpp::List rr(Rcpp::Pairlist(Rcpp::Named("par", par),
-				 Rcpp::Named("fval",
-					     F77_NAME(calfun)(&n,
-							      par.begin(),
-							      ip.begin())),
-				 Rcpp::Named("feval", rho.get(".feval.")),
-				 Rcpp::Named("intpval", fval)));
-    rr.attr("class") = "bobyqa";
-    return rr;
+    double rb = Rcpp::as<double>(cc.get("rhobeg")),
+	re = Rcpp::as<double>(cc.get("rhoend"));
+    int ip = Rcpp::as<int>(cc.get("iprint")),
+	mxf = Rcpp::as<int>(cc.get("maxfun")),
+	n = par.size(), np = Rcpp::as<int>(cc.get("npt"));
+    std::vector<double> w((np + 5) * (np + n) + (3 * n * (n + 5))/2);
+    
+    F77_NAME(bobyqa)(&n, &np, par.begin(), xl.begin(), xu.begin(),
+		     &rb, &re, &ip, &mxf, &w[0]);
+    return rval(par, "bobyqa");
 }
 
 extern "C" 
@@ -84,29 +84,20 @@ void F77_NAME(uobyqa)(const int *n, double X[],
 		      const double *rhobeg, const double *rhoend,
 		      const int *iprint, const int *maxfun, double w[]);
 
-RcppExport SEXP uobyqa_cpp(SEXP ppar, SEXP pctrl, SEXP pfn, SEXP work)
-{
+RcppExport SEXP uobyqa_cpp(SEXP ppar, SEXP pctrl, SEXP pfn) {
     Rcpp::Environment cc(pctrl); 
-    Rcpp::NumericVector par(ppar), rb(cc.get("rhobeg")), re(cc.get("rhoend")),
-	wrk(work);
-    Rcpp::IntegerVector mxf(cc.get("maxfun")), ip(cc.get("iprint"));
+    Rcpp::NumericVector par(ppar);
     cf = Rcpp::Function(pfn);
+    double rb = Rcpp::as<double>(cc.get("rhobeg")),
+	re = Rcpp::as<double>(cc.get("rhoend"));
+    int ip = Rcpp::as<int>(cc.get("iprint")),
+	mxf = Rcpp::as<int>(cc.get("maxfun")), n = par.size();
     Rcpp::Environment rho(cf.environment());
-    int n = par.size();
+    std::vector<double>
+	w((n*(42+n*(23+n*(8+n))) + std::max(2*n*n + 4, 18*n)) / 4);
 
-    if (!(rb.size() && re.size() && mxf.size() && ip.size()))
-	Rf_error("Incomplete control parameter list");
-
-    F77_NAME(uobyqa)(&n, par.begin(), rb.begin(), re.begin(),
-		     ip.begin(), mxf.begin(), wrk.begin());
-
-    Rcpp::List rr(Rcpp::Pairlist(Rcpp::Named("par", par),
-				 Rcpp::Named("fval",
-					     F77_NAME(calfun)(&n, par.begin(),
-							      ip.begin())),
-				 Rcpp::Named("feval", rho.get(".feval."))));
-    rr.attr("class") = "uobyqa";
-    return rr;
+    F77_NAME(uobyqa)(&n, par.begin(), &rb, &re, &ip, &mxf, &w[0]);
+    return rval(par, "uobyqa");
 }
 
 extern "C" 
@@ -114,35 +105,23 @@ void F77_NAME(newuoa)(const int *n, const int *npt, double X[],
 		      const double *rhobeg, const double *rhoend,
 		      const int *iprint, const int *maxfun, double w[]);
 
-RcppExport SEXP newuoa_cpp(SEXP ppar, SEXP pctrl, SEXP pfn, SEXP work)
-{
+RcppExport SEXP newuoa_cpp(SEXP ppar, SEXP pctrl, SEXP pfn) {
     Rcpp::Environment cc(pctrl); 
-    Rcpp::NumericVector par(ppar), rb(cc.get("rhobeg")),
-	re(cc.get("rhoend")), wrk(work);
-    Rcpp::IntegerVector mxf(cc.get("maxfun")), npt(cc.get("npt")),
-	ip(cc.get("iprint"));
-    int n = par.size();
+    Rcpp::NumericVector par(ppar);
+    double rb = Rcpp::as<double>(cc.get("rhobeg")),
+	re = Rcpp::as<double>(cc.get("rhoend"));
+    int ip = Rcpp::as<int>(cc.get("iprint")),
+	mxf = Rcpp::as<int>(cc.get("maxfun")),
+	n = par.size(), np = Rcpp::as<int>(cc.get("npt"));
+    std::vector<double> w((np+13)*(np+n)+(3*n*(n+3))/2);
     cf = Rcpp::Function(pfn);
-    Rcpp::Environment rho(cf.environment());
 
-    if (!(rb.size() && npt.size() && re.size() && mxf.size() && ip.size()))
-	Rf_error("Incomplete control parameter list");
-
-    F77_NAME(newuoa)(&n, npt.begin(), par.begin(), rb.begin(), re.begin(),
-		     ip.begin(), mxf.begin(), wrk.begin());
-
-    Rcpp::List rr(Rcpp::Pairlist(Rcpp::Named("par", par),
-				 Rcpp::Named("fval",
-					     F77_NAME(calfun)(&n, par.begin(),
-							      ip.begin())),
-				 Rcpp::Named("feval", rho.get(".feval."))));
-    rr.attr("class") = "uobyqa";
-    return rr;
+    F77_NAME(newuoa)(&n, &np, par.begin(), &rb, &re, &ip, &mxf, &w[0]);
+    return rval(par, "newuoa");
 }
 
 /// Assorted error messages.
-extern "C" void F77_NAME(minqer)(const int *msgno)
-{
+extern "C" void F77_NAME(minqer)(const int *msgno) {
     const char *msg;
     switch(*msgno) {
     case 10:
@@ -173,8 +152,7 @@ extern "C" void F77_NAME(minqer)(const int *msgno)
 extern "C" void
 F77_NAME(minqit)(const int *iprint, const double *rho, const int *nf,
 		 const double *fopt, const int *n, const double xbase[],
-		 const double xopt[])
-{
+		 const double xopt[]) {
     if (*iprint >= 2) {
 	Rprintf("%#8.2g; %3d: %#14.8g,", *rho, *nf, *fopt);
 	for(int i = 0; i < *n; i++) Rprintf("%#8g ", xbase[i] + xopt[i]);
@@ -185,8 +163,7 @@ F77_NAME(minqit)(const int *iprint, const double *rho, const int *nf,
 /// Output at return (do we really need this - why not use the print method?)
 extern "C" void
 F77_NAME(minqir)(const int *iprint, const double *f, const int *nf,
-		 const int *n, const double x[])
-{
+		 const int *n, const double x[]) {
     if (*iprint > 0) {
 	Rprintf("At return\n");
 	Rprintf("%3d:%#14.8g:", *nf, *f);
@@ -194,3 +171,21 @@ F77_NAME(minqir)(const int *iprint, const double *f, const int *nf,
 	Rprintf("\n");
     }
 }
+
+#define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
+
+static R_CallMethodDef CallEntries[] = {
+    CALLDEF(bobyqa_cpp, 5),
+    CALLDEF(uobyqa_cpp, 3),
+    CALLDEF(newuoa_cpp, 3),
+    {NULL, NULL, 0}
+};
+
+/// Initializer for the package.  Registers the symbols for .Call.
+extern "C" void R_init_minqa(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
+
+
+

3a109636ced6c359e5ba5424947c3126ad4cf8a6
nashjc (Prof ) <nashjc@uottawa.ca>
2011-07-06 23:06:19 +0000

    Tidied packages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@438 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rvmmin/R/optansout.R
+++ pkg/Rvmmin/R/optansout.R
@@ -1,36 +1,44 @@
 optansout <- function(ansdf, filename) {
-##### OPEN ISSUES: (any date order)
-
-##### IMPLEMENTED: (reverse date order)
-
-#  A funtion to display and print to file (if present) the output of optimx
-	if (! is.null(filename)) { sink(filename) } # may need to check paths
-#	if (! exists(filename)) { sink(filename) } # may need to check paths
-	tpar<-ansdf$par
-	tdf<-ansdf
-        ltvec<-length(tpar[[1]])
-	for (i in 1:length(tpar)) {
-	    tvec<-tpar[[i]]
-	    ltvec<-length(tvec)
-	    if (ltvec > 5) {
-		tvec<-tvec[1:5]
-	    }
-	    tpar[[i]]<-tvec
-	}
-	tdf$par<-tpar
-	pardf<-tdf[1]
-	if (ltvec > 5) {
-		names(pardf)[1]<-"first.5.par"
-	} else {
-		names(pardf)[1]<-"par"
-	}
-	print(pardf)	
-	tdf$par<-NULL
-	print(tdf)
-	if (! is.null(filename)) { sink() } # to turn it off
-#	if (! exists(filename)) { sink() } # to turn it off
-	rm(tdf)
-	rm(tpar)
-	return(TRUE)
-} ## end of optansout
-
+    ##### OPEN ISSUES: (any date order)
+    
+    ##### IMPLEMENTED: (reverse date order)
+    
+    # A funtion to display and print to file (if present) the
+    #   output of optimx
+    if (!is.null(filename)) 
+        {
+            sink(filename)
+        }  # may need to check paths
+    # if (! exists(filename)) { sink(filename) } # may need to
+    #   check paths
+    tpar <- ansdf$par
+    tdf <- ansdf
+    ltvec <- length(tpar[[1]])
+    for (i in 1:length(tpar)) {
+        tvec <- tpar[[i]]
+        ltvec <- length(tvec)
+        if (ltvec > 5) {
+            tvec <- tvec[1:5]
+        }
+        tpar[[i]] <- tvec
+    }
+    tdf$par <- tpar
+    pardf <- tdf[1]
+    if (ltvec > 5) {
+        names(pardf)[1] <- "first.5.par"
+    }
+    else {
+        names(pardf)[1] <- "par"
+    }
+    print(pardf)
+    tdf$par <- NULL
+    print(tdf)
+    if (!is.null(filename)) 
+        {
+            sink()
+        }  # to turn it off
+    #\tif (! exists(filename)) { sink() } # to turn it off
+    rm(tdf)
+    rm(tpar)
+    return(TRUE)
+}  ## end of optansout

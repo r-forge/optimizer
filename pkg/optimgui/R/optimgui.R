9a601d270d9706da97d16143c337ef374959f45c
yixuan (Yixuan Qiu) <yixuanq@yahoo.com.cn>
2011-07-08 01:40:32 +0000

    Move event functions into event.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@444 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optimgui/R/optimgui.R
+++ pkg/optimgui/R/optimgui.R
@@ -1,155 +1,12 @@
-optimgui = function()
-{
-options("guiToolkit" = "RGtk2");
-
-# Rop file parser
-"%+%" = function(x, y) sprintf("%s%s", x, y);
-parseRop = function(filePath)
-{
-	RopFile = readLines(filePath);
-	RopFile = RopFile[RopFile != ""];
-	index.XML = grep("^#@", RopFile);
-	index.rcode = (1:length(RopFile))[-index.XML];
-	RopFile[index.XML] = gsub("^#@ ", "", RopFile[index.XML]);
-	RopFile[index.rcode] = gsub("<", "&lt;", RopFile[index.rcode]);
-	RopFile[index.rcode] = gsub(">", "&gt;", RopFile[index.rcode]);
-	RopTree = xmlTreeParse(RopFile, asText = TRUE);
-	return(RopTree);
-}
-writeRop = function(filePath)
-{
-	Rop = xmlTree("Roptimgui");
-	for(i in 1:length(tabsList))
-	{
-		Rop$addNode("tab", attrs = c(tabname = names(tabsList)[i]),
-				    close = FALSE);
-		for(j in 1:length(tabsList[[i]]))
-		{
-            tag = names(tabsList[[i]])[j];
-            textValue = svalue(tabsList[[i]][[j]]);
-            if(tag == "label")
-            {
-                textValue = strwrap(textValue, 70);
-                textValue = "\n" %+% paste("    ", textValue, sep = "",
-                                           collapse = "\n") %+% "\n    ";
-            }else if(tag == "rcode")
-            {
-                textValue = gsub("\n", "\n##Rcode##", textValue);
-                textValue = "\n##Rcode##" %+% textValue %+% "\n    ";
-            }
-			Rop$addNode(tag, textValue);
-		}
-		Rop$closeTag();
-	}
-    z = textConnection("buffer", open = "w");
-    sink(z);
-	cat(saveXML(Rop$value()));
-    sink();
-    close(z);
-    buffer = paste("#@ ", buffer, sep = "");
-    buffer = gsub("#@ ##Rcode##", "", buffer);
-	buffer = gsub("&lt;", "<", buffer);
-	buffer = gsub("&gt;", ">", buffer);
-    writeLines(buffer, filePath);
-}
-# Add the content of a tab to a notebook widget
-addTabs = function(tabNode, noteBookWidget)
-{
-	tabBox = ggroup(horizontal = FALSE, container = noteBookWidget,
-			        label = xmlAttrs(tabNode)["tabname"], spacing = 15);
-	addWidgets = function(widgetNode, boxWidget)
-	{
-		if(xmlName(widgetNode) == "title")
-		{
-			titleLabel = glabel(xmlValue(widgetNode), container = boxWidget);
-			font(titleLabel) = c(size = "xx-large");
-			return(titleLabel);
-		}else if(xmlName(widgetNode) == "label"){
-			labelContent = gsub("\n +", " ", xmlValue(widgetNode));
-			labelContent = paste(labelContent, collapse = "");
-			docLabel = gtextbox(labelContent, container = boxWidget,
-					            font.attr = c(family = "sans", size = 11));
-			return(docLabel);
-		}else if(xmlName(widgetNode) == "rcode"){
-			codeText = gtextbox(xmlValue(widgetNode), container = boxWidget,
-					            font.attr = c(family = "monospace", size = 11),
-								frame = TRUE);
-			return(codeText);
-		}
-	}
-	return(xmlApply(tabNode, addWidgets, boxWidget = tabBox));
-}
-# Open Rop file
-currentFile = NULL;
-openRopFile = function(filePath)
-{
-	visible(buttonGroup) = FALSE;
-	visible(noteBook) = FALSE;
-	while(dispose(noteBook)){}
-	path = filePath;
-	Encoding(path) = "UTF-8";
-	RopTree = parseRop(path);
-	currentFile <<- path;
-	tabsList <<- xmlApply(xmlRoot(RopTree), addTabs, noteBookWidget = noteBook);
-	names(tabsList) <<- xmlSApply(xmlRoot(RopTree), xmlAttrs);
-	visible(noteBook) = TRUE;
-	visible(buttonGroup) = TRUE;
-    enabled(mSave) = TRUE;
-	return(NULL);
-}
-
+optimgui = function(){
 
 # Main Window
 mainWin = gwindow("optimgui", visible = FALSE);
 size(mainWin) = c(800, 600);
 # Horizontal layout box
 hGroup = ggroup(container = mainWin, expand = TRUE);
-# Notebook widget
-noteBook = gnotebook(closebuttons = FALSE, container = hGroup, expand = TRUE);
-noteBook@widget@widget$modifyBg(GtkStateType["normal"],
-		                         gdkColorParse("white")$color);
-
-# Welcome page
-welcomePageNew = function()
-{
-	welcomePage = ggroup(horizontal = FALSE, expand = TRUE);
-	welcomeLabel = glabel("Create a new project or open an existing Rop file.",
-			container = welcomePage);
-	font(welcomeLabel) = c(size = "x-large");
-	tmpBox1 = ggroup(container = welcomePage);
-	tmpBox2 = ggroup(container = welcomePage);
-	tmpBox3 = ggroup(container = welcomePage);
-	gimage(system.file("resources", "images",  "wizard.png", package = "optimgui"), container = tmpBox1);
-	wizardLabel = glinklabel("Create a new project by wizard");
-	add(tmpBox1, wizardLabel);
-	gimage(system.file("resources", "images",  "template.png", package = "optimgui"), container = tmpBox2);
-	newLabel = glinklabel("Create a new project with template");
-	add(tmpBox2, newLabel);
-	gimage(system.file("resources", "images",  "open.png", package = "optimgui"), container = tmpBox3);
-	openLabel = glinklabel("Open an existing Rop file");
-	add(tmpBox3, openLabel);
-	rtvalue = list(welcomePage = welcomePage, wizardLabel = wizardLabel,
-			newLabel = newLabel, openLabel = openLabel);
-	welcomePageAddEvent(rtvalue);
-	return(rtvalue);
-}
-
-# Catalog page
-catalogPageNew = function()
-{
-	catalogPage = ggroup(horizontal = FALSE, expand = TRUE);
-	# FIXME
-	Ropfiles = data.frame(FileName = "shobbs.Rop",
-			Description = "Scaled Hobbs weeds problem");
-	RopTable = gtable(Ropfiles, container = catalogPage, expand = TRUE);
-	gseparator(container = catalogPage);
-	tmpBox4 = ggroup(container = catalogPage);
-	openRopButton = gbutton("OK", container = tmpBox4);
-	rtvalue = list(catalogPage = catalogPage, RopTable = RopTable,
-			openRopButton = openRopButton);
-	catalogPageAddEvent(rtvalue);
-	return(rtvalue);
-}
+# The editor
+editor <<- EditorNew(container = hGroup);
 
 # Wizard page. This part is programmed using RGtk2.
 wizardPage = gtkAssistantNew(show = FALSE);
@@ -184,121 +41,22 @@ wizardPage$setPageType(wizardConfirmPage, GtkAssistantPageType["confirm"]);
 wizardPage$setPageComplete(wizardConfirmPage, TRUE);
 
 # Button box
-buttonGroup = ggroup(container = hGroup, horizontal = FALSE);
+buttonGroup <<- ggroup(container = hGroup, horizontal = FALSE);
 visible(buttonGroup) = FALSE;
 # Buttons
 runButton = gbutton("Run", container = buttonGroup);
 synButton = gbutton("Syntex check", container = buttonGroup);
 anoButton = gbutton("Another", container = buttonGroup);
 
-# Events
-# Exit
-onExit = function(h, ...) dispose(mainWin);
-# Open wizard
-onOpenWizard = function(h, ...)
-{
-	wizardPage$show();
-	return(NULL);
-}
-# Wizard confirmed
-onWizardConfirmed = function(widget, param)
-{
-	widget$hide();
-	fileName = "shobbs.Rop";
-	filePath = system.file("resources", "Rop", fileName, package = "optimgui");
-	if(!length(filePath)) return(NULL);
-	openRopFile(filePath);
-	return(FALSE);
-}
-# Open Catalog Page event
-onOpenCatalog = function(h, ...)
-{
-	add(noteBook, catalogPageNew()$catalogPage, label = "Catalog");
-	return(NULL);
-}
-# Default event
-onDefaultEvent = function(h, ...) gmessage("Not implemented yet. :(", "Message");
-# Open built-in Rop file
-onOpenBuiltinRopFile = function(h, ...)
-{
-	fileName = as.character(svalue(h$action$RopTable));
-	filePath = system.file("resources", "Rop", fileName, package = "optimgui");
-	if(!length(filePath)) return(NULL);
-	openRopFile(filePath);
-	return(NULL);
-}
-# Open Rop file event
-onOpenRopFile = function(h, ...)
-{
-	filePath = gfile(text = "Choose an Rop file...", type = "open",
-			         filter = list("Rop files (*.Rop)" = list(patterns = "*.Rop")),
-			         handler = function(h,...) return(NULL));
-	if(is.na(filePath)) return(NULL);
-	openRopFile(filePath);
-	return(NULL);
-}
-# Save Rop file event
-onSaveRopFile = function(h, ...)
-{
-    filePath = gfile(text = "Save an Rop file...", type = "save",
-			         filter = list("Rop files (*.Rop)" = list(patterns = "*.Rop")),
-			         handler = function(h,...) return(NULL));
-	if(is.na(filePath)) return(NULL);
-    Encoding(filePath) = "UTF-8";
-	writeRop(filePath %+% ".Rop");
-	return(NULL);
-}
-tabsList = list();
-# Close Rop file event
-onCloseRopFile = function(h, ...)
-{
-	visible(buttonGroup) = FALSE;
-	visible(noteBook) = FALSE;
-	while(dispose(noteBook)){}
-	currentFile <<- NULL;
-	add(noteBook, welcomePageNew()$welcomePage, label = "Welcome");
-	visible(noteBook) = TRUE;
-    enabled(mSave) = FALSE;
-	return(NULL);
-}
-# Run code event
-onRunCode = function(h, ...)
-{
-	if(!is.null(currentFile))
-	{
-		z = textConnection("output", open = "w");
-		sink(z);
-		source(currentFile, echo = FALSE, print.eval = TRUE);
-		sink();
-		close(z);
-		output = paste(output, collapse = "\n");
-		outputWidget = tabsList[["Run"]]$rcode;
-		svalue(outputWidget) = paste(svalue(outputWidget), output, sep = "\n");
-        svalue(noteBook) = which(names(tabsList) == "Run");
-	}
-}
-
 # Add events
-welcomePageAddEvent = function(welcomePage)
-{
-	addHandlerClicked(welcomePage$wizardLabel, onOpenWizard);
-	addHandlerClicked(welcomePage$newLabel, onOpenCatalog);
-	addHandlerClicked(welcomePage$openLabel, onOpenRopFile);
-}
-welcomePage = welcomePageNew();
-add(noteBook, welcomePage$welcomePage, label = "Welcome");
-catalogPageAddEvent = function(catalogPage)
-{
-	addHandlerClicked(catalogPage$openRopButton, handler = onOpenBuiltinRopFile,
-			action = catalogPage);
-}
+editor = showWelcomePage.Editor(editor);
 gSignalConnect(wizardPage, "apply", onWizardConfirmed, NULL);
 addHandlerClicked(runButton, onRunCode);
 
 
 # Menu list
 mOpen = gaction(label = "Open", handler = onOpenRopFile, icon = "open");
-mSave = gaction(label = "Save", handler = onSaveRopFile, icon = "save");
+mSave <<- gaction(label = "Save", handler = onSaveRopFile, icon = "save");
 mClose = gaction(label = "Close", handler = onCloseRopFile, icon = "close");
 mExit = gaction(label = "Exit", handler = onExit, icon = "quit");
 mCopy = gaction(label = "Copy", handler = onDefaultEvent);

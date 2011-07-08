4fb0f4878117f4280308f60b199389400cba24c8
yixuan (Yixuan Qiu) <yixuanq@yahoo.com.cn>
2011-07-08 07:55:11 +0000

    Fix most event bugs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@452 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optimgui/R/events.R
+++ pkg/optimgui/R/events.R
@@ -1,10 +1,15 @@
+# Passing parameters
+# h$action = param = list(mainEnvir = environment(), mainWin = mainWin,
+#                         wizardPage = wizardPage, buttonGroup = buttonGroup,
+#                         mSave = mSave);
+
 # Events
 # Exit
-onExit = function(h, ...) dispose(mainWin);
+onExit = function(h, ...) dispose(h$action$mainWin);
 # Open wizard
 onOpenWizard = function(h, ...)
 {
-    wizardPage$show();
+    h$action$wizardPage$show();
 	return(NULL);
 }
 # Wizard confirmed
@@ -14,36 +19,39 @@ onWizardConfirmed = function(widget, param)
 	fileName = "shobbs.Rop";
 	filePath = system.file("resources", "Rop", fileName, package = "optimgui");
 	if(!length(filePath)) return(NULL);
-	openRopFile(filePath);
+	openRopFile(filePath, param);
 	return(FALSE);
 }
 # Open Catalog Page event
 onOpenCatalog = function(h, ...)
 {
-    showCatalogPage.Editor(editor);
+    editor = get("editor", envir = h$action$mainEnvir);
+    showCatalogPage(editor, h$action);
 	return(NULL);
 }
 # Default event
 onDefaultEvent = function(h, ...) gmessage("Not implemented yet. :(", "Message");
-# Open built-in Rop file
 # Open Rop file
-openRopFile = function(filePath)
+openRopFile = function(filePath, param)
 {
-    visible(buttonGroup) = FALSE;
+    editor = get("editor", envir = param$mainEnvir);
+    visible(param$buttonGroup) = FALSE;
     path = filePath;
 	Encoding(path) = "UTF-8";
-    editor <<- openRopFile.Editor(editor, path);
-    editor <<- buildWidgets.Editor(editor);
-	visible(buttonGroup) = TRUE;
-    enabled(mSave) = TRUE;
+    editor = loadRopFile(editor, path);
+    buildWidgets(editor);
+    assign("editor", editor, envir = param$mainEnvir);
+	visible(param$buttonGroup) = TRUE;
+    enabled(param$mSave) = TRUE;
 	return(NULL);
 }
+# Open built-in Rop file
 onOpenBuiltinRopFile = function(h, ...)
 {
-	fileName = as.character(svalue(h$action$RopTable));
+	fileName = as.character(svalue(h$action$catalogPage$RopTable));
 	filePath = system.file("resources", "Rop", fileName, package = "optimgui");
 	if(!length(filePath)) return(NULL);
-	openRopFile(filePath);
+	openRopFile(filePath, h$action$param);
 	return(NULL);
 }
 # Open Rop file event
@@ -53,32 +61,36 @@ onOpenRopFile = function(h, ...)
 			         filter = list("Rop files (*.Rop)" = list(patterns = "*.Rop")),
 			         handler = function(h,...) return(NULL));
 	if(is.na(filePath)) return(NULL);
-	openRopFile(filePath);
+	openRopFile(filePath, h$action);
 	return(NULL);
 }
 # Save Rop file event
 onSaveRopFile = function(h, ...)
 {
+    editor = get("editor", envir = h$action$mainEnvir);
     filePath = gfile(text = "Save an Rop file...", type = "save",
 			         filter = list("Rop files (*.Rop)" = list(patterns = "*.Rop")),
 			         handler = function(h,...) return(NULL));
 	if(is.na(filePath)) return(NULL);
     Encoding(filePath) = "UTF-8";
-	saveRopFile.Editor(editor, filePath %+% ".Rop");
+	saveRopFile(editor, filePath %+% ".Rop");
 	return(NULL);
 }
 # Close Rop file event
 onCloseRopFile = function(h, ...)
 {
-	visible(buttonGroup) = FALSE;
-    editor <<- clearAll.Editor(editor);
-    editor <<- showWelcomePage.Editor(editor);
-    enabled(mSave) = FALSE;
+    editor = get("editor", envir = h$action$mainEnvir);
+	visible(h$action$buttonGroup) = FALSE;
+    editor = clearAll(editor);
+    showWelcomePage(editor, h$action);
+    assign("editor", editor, envir = h$action$mainEnvir);
+    enabled(h$action$mSave) = FALSE;
 	return(NULL);
 }
 # Run code event
 onRunCode = function(h, ...)
 {
+    editor = get("editor", envir = h$action$mainEnvir);
 	if(length(editor@currentFile))
 	{
 		z = textConnection("output", open = "w");
@@ -95,15 +107,15 @@ onRunCode = function(h, ...)
     return(NULL);
 }
 
-welcomePageAddEvent = function(welcomePage)
+welcomePageAddEvent = function(welcomePage, param)
 {
-    addHandlerClicked(welcomePage$wizardLabel, onOpenWizard);
-	addHandlerClicked(welcomePage$newLabel, onOpenCatalog);
-	addHandlerClicked(welcomePage$openLabel, onOpenRopFile);
+    addHandlerClicked(welcomePage$wizardLabel, onOpenWizard, param);
+	addHandlerClicked(welcomePage$newLabel, onOpenCatalog, param);
+	addHandlerClicked(welcomePage$openLabel, onOpenRopFile, param);
 }
-catalogPageAddEvent = function(catalogPage)
+catalogPageAddEvent = function(catalogPage, param)
 {
-    addHandlerClicked(catalogPage$openRopButton, handler = onOpenBuiltinRopFile,
-                   action = catalogPage);
+    addHandlerClicked(catalogPage$openRopButton, onOpenBuiltinRopFile,
+                      action = list(catalogPage = catalogPage, param = param));
 }
 

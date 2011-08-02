114bd923aa1e96312abb3c98a04c99edad0296e3
yixuan (Yixuan Qiu) <yixuanq@yahoo.com.cn>
2011-08-02 04:18:57 +0000

    Remove wizard option
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@486 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optimgui/R/optimgui.R
+++ pkg/optimgui/R/optimgui.R
@@ -15,44 +15,11 @@ optimgui = function(){
 
 # Main Window
 mainWin = gwindow("optimgui", visible = FALSE);
-size(mainWin) = c(800, 600);
+size(mainWin) = c(900, 600);
 # Horizontal layout box
 hGroup = ggroup(container = mainWin, expand = TRUE, spacing = 15);
 # The editor
 editor = EditorNew(container = hGroup);
-
-# Wizard page. This part is programmed using RGtk2.
-wizardPage = gtkAssistantNew(show = FALSE);
-wizardPage$setTitle("Wizard");
-wizardPage$setDefaultSize(640, 480);
-# Welcome page
-wizardWelcomePage = gtkLabelNew("In this wizard you will be asked several questions about your optimization problem,
-				and in the final step a suggested template will be provided.");
-wizardWelcomePage$modifyFont(pangoFontDescriptionFromString("sans 11"));
-wizardWelcomePage$setAlignment(0.2, 0.1);
-wizardPage$appendPage(wizardWelcomePage);
-wizardPage$setPageTitle(wizardWelcomePage, "Welcome");
-wizardPage$setPageComplete(wizardWelcomePage, TRUE);
-# Question 1, just a demo
-wizardQ1 = gtkVBoxNew();
-wizardQ1$packStart(gtkLabelNew("Do you xxxxx ?"));
-radioYes = gtkRadioButtonNewWithLabel(NULL, "YES");
-radioNo = gtkRadioButtonNewWithLabelFromWidget(radioYes, "NO");
-wizardQ1$packStart(radioYes);
-wizardQ1$packStart(radioNo);
-wizardPage$appendPage(wizardQ1);
-wizardPage$setPageTitle(wizardQ1, "Question 1");
-wizardPage$setPageComplete(wizardQ1, TRUE);
-# Final step
-wizardConfirmPage = gtkLabelNew("According to your selections, the suggested template is xxx.
-				Press 'Apply' to confirm.");
-wizardConfirmPage$modifyFont(pangoFontDescriptionFromString("sans 11"));
-wizardConfirmPage$setAlignment(0.2, 0.1);
-wizardPage$appendPage(wizardConfirmPage);
-wizardPage$setPageTitle(wizardConfirmPage, "Confirmation");
-wizardPage$setPageType(wizardConfirmPage, GtkAssistantPageType["confirm"]);
-wizardPage$setPageComplete(wizardConfirmPage, TRUE);
-
 # Button box
 buttonGroup = ggroup(container = hGroup, horizontal = FALSE);
 visible(buttonGroup) = FALSE;
@@ -69,12 +36,11 @@ codeCheckBox = gcheckbox("Show code", checked = TRUE, container = buttonGroup);
 # Parameters to be passed to event handlers
 # "mSave" must be created before "param" is assigned.
 mSave = gaction(label = "Save", handler = onSaveRopFile,
-                action = list(editor = editor), icon = "save");
-param = list(mainWin = mainWin, editor = editor, wizardPage = wizardPage,
+                action = list(mainWin = mainWin, editor = editor), icon = "save");
+param = list(mainWin = mainWin, editor = editor,
              buttonGroup = buttonGroup, mSave = mSave,
              noteCheckBox = noteCheckBox, codeCheckBox = codeCheckBox);
 editor$showWelcomePage(param);
-gSignalConnect(wizardPage, "apply", onWizardConfirmed, param);
 addHandlerFocus(editor$noteBook, onFocusTab, param);
 addHandlerClicked(addButton, onAddTab, param);
 addHandlerClicked(deleteButton, onDeleteTab, param);

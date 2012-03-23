a1cb23125707781b514b2fd9b24960ebf872eedd
nashjc (Prof ) <nashjc@uottawa.ca>
2012-03-23 17:51:10 +0000

    updates to nlmrt
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@583 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nlmrt/R/wrapnls.R
+++ pkg/nlmrt/R/wrapnls.R
@@ -1,14 +1,29 @@
-wrapnls <-function(formula, start, trace=FALSE, data=NULL, control=list(),...){
+wrapnls <-function(formula, start, trace=FALSE, data=NULL, lower=NULL, upper=NULL, masked=NULL, control=list(),...){
 #
 #  A wrapper to call nlsmnq() and then call nls() with the solution.
 #  The calling sequence matches that of nlsmnq()
 #
     if (is.null(data)) stop("wrapnls() must have 'data' supplied")
-
-    first<-nlsmnq(formula, start, trace=trace, data=data, control=control,...)
-#  Should check this has worked, but ...
-    newstart<-first$coeffs
-    print(first)
-#  Should put this in a try(), but let's see it work first
-    second<-nls(formula, newstart, trace=trace, data=data, control=control,...)
+    if (!is.null(masked)) stop("nls() cannot handle masked parameters")
+    if (is.null(lower) && is.null(upper)) bounded<-FALSE else bounded<-TRUE
+    pnames<-names(start)
+    if (bounded) { # deal with bounds constrained nonlinear least squares
+       first<-nlsmnqb(formula, start, trace=trace, data=data, lower=lower,
+                       upper=upper, control=control,...)
+       #  Should check this has worked, but ...
+       newstart<-first$coeffs
+       names(newstart)<-pnames
+       print(first)
+       #  Should put this in a try(), but let's see it work first
+       second<-nls(formula, newstart, trace=trace, data=data, 
+              algorithm='port', lower=lower, upper=upper, control=control,...)
+     } else {
+       first<-nlsmnq(formula, start, trace=trace, data=data, control=control,...)
+       #  Should check this has worked, but ...
+       newstart<-first$coeffs
+       names(newstart)<-pnames
+       print(first)
+       #  Should put this in a try(), but let's see it work first
+       second<-nls(formula, newstart, trace=trace, data=data, control=control,...)
+     }
 }

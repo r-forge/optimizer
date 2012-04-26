783e28172a52c3afba410fd1eb7cbcdd53c95dfb
nashjc (Prof ) <nashjc@uottawa.ca>
2012-04-26 20:18:54 +0000

    To update nlmrt with model to function routines
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@602 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nlmrt/R/model2grfun.R
@@ -0,0 +1,83 @@
+model2grfun <- function(resformula, pvec, filename) {
+   pnames<-names(pvec)
+#   cat("pnames:")
+#   print(pnames)
+   if (is.null(pnames) ) stop("MUST have named parameters in pvec")
+   if (is.character(resformula)){
+      es<-resformula
+   } else {
+      tstr<-as.character(resformula) # note ordering of terms!
+      es<-paste(tstr[[2]],"~",tstr[[3]],'')
+   }
+   xx <- all.vars(parse(text=es))
+#   cat("xx:")
+#   print(xx)
+   rp <- match(pnames, xx) # Match names to parameters
+# ?? How to ensure there are names?
+   xx2 <- c(xx[rp], xx[-rp])
+   xxparm<-xx[rp]
+   cat("xx2:")
+   print(xx2)
+   cat("xxparm:")
+   print(xxparm)
+   pstr<-"c("
+   npar<-length(xxparm)
+   if (npar>0) {
+      for (i in 1:npar){
+         pstr<-paste(pstr,'"',xxparm[i],'"', sep='')
+         if (i<npar) pstr<-paste(pstr,", ",sep='')
+      }
+   }
+   pstr<-paste(pstr,")",sep='')
+   cat("pstr:")
+   print(pstr)
+   tmp<-readline("...")
+   xxvars<-xx[-rp]
+   nvar<-length(xxvars)
+   vstr<-""
+   if (nvar>0) {
+      for (i in 1:nvar){
+         vstr<-paste(vstr,xxvars[i]," = NULL", sep='')
+         if (i<nvar) vstr<-paste(vstr,", ",sep='')
+      }
+   }
+   ff <- vector("list", length(xx2))
+   names(ff) <- xx2
+   parts<-strsplit(as.character(es), "~")[[1]]
+   if (length(parts)!=2) stop("Model expression is incorrect!")
+   lhs<-parts[1]
+   rhs<-parts[2]
+#  And build the residual at the parameters
+   resexp<-paste(rhs,"-",lhs, collapse=" ") # build the residuals
+   resval<-paste("resids<-as.numeric(eval(",resexp,"))", sep="") ##3
+   resexp<-paste(rhs,"-",lhs, collapse=" ") # build the residuals
+   jacexp<-deriv(parse(text=resexp), pnames) # gradient expression
+#   cat("jacexp:")
+#   print(jacexp)
+#   tmp<-readline("cont.")
+   dvstr<-""
+   if (nvar>0) {
+      for (i in 1:nvar){
+         dvstr<-paste(dvstr,xxvars[i], sep='')
+         if (i<nvar) dvstr<-paste(dvstr,", ",sep='')
+      }
+   }
+   jfstr<-paste("localdf<-data.frame(",dvstr,");\n", sep='')
+   jfstr<-paste(jfstr,"jstruc<-with(localdf,eval(",jacexp,"))", sep="") ##3
+   pparse<-""
+   for (i in 1:npar){
+      pparse<-paste(pparse, "   ",pnames[[i]],"<-prm[[",i,"]]\n", sep='')
+   }
+   myjstr<-paste("myjac<-function(prm, ",vstr,"){\n",
+      pparse,"\n ",
+      jfstr," \n",
+      "jacmat<-attr(jstruc,'gradient')\n ",
+      resval,"\n",
+      "grj<-t(jacmat) %*% resids \n",
+      "}",sep='')
+   write(myjstr, file=filename) # write out the file
+   myjac<-source(file=filename)$value # This may be inefficient, but ...
+#   attr(myjac, "source-text")<-myjstr
+   return(myjac)      
+}
+
---
+++ pkg/nlmrt/R/modgr.R
@@ -0,0 +1,10 @@
+modgr<-function(prm, resfn, jacfn, ...){
+  # computes the gradient J' %*% res residuals
+  # (res) and jacobian (Jac) defined by resfn 
+  # and jacfn at parameters prm and extra
+  # variables defined in the dot-arguments
+  # J C Nash 2012-4-26
+  res<-resfn(prm, ...) # ?? try()
+  Jac<-jacfn(prm, ...)
+  grj<-t(Jac) %*% res
+}

38130604be5a6ada4101be9bdfe93c1edf9ec2dd
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2013-10-23 17:02:47 +0000

    added one-sided first derivatives to grad and jacobian.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@785 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/numDeriv/R/numDeriv.R
+++ pkg/numDeriv/R/numDeriv.R
@@ -1,13 +1,20 @@
+  # grad case 1 and 2 are special cases of jacobian, with a scalar rather than
+  #   vector valued function. Case 3 differs only because of the interpretation
+  #   that the vector result is a scalar function applied to each argument, and the
+  #   thus the result has the same length as the argument.
+  #   The code of grad could be consolidated to use jacobian.
+  #   There is also some duplication in genD.
+
 ############################################################################
 
 #    functions for gradient calculation
 
 ############################################################################
 
-grad <- function (func, x, method="Richardson", method.args=list(), ...)
-   UseMethod("grad")
+grad <- function (func, x, method="Richardson", side=NULL, 
+   method.args=list(), ...) UseMethod("grad")
 
-grad.default <- function(func, x, method="Richardson",
+grad.default <- function(func, x, method="Richardson", side=NULL,
       method.args=list(), ...){
   # modified by Paul Gilbert from code by Xingqiao Liu.
   # case 1/ scalar arg, scalar result (case 2/ or 3/ code should work)
@@ -15,25 +22,41 @@ grad.default <- function(func, x, method="Richardson",
   # case 3/ vector arg, vector result (of same length, really 1/ applied multiple times))
   f <- func(x, ...)
   n <- length(x)	 #number of variables in argument
+
+  if (is.null(side)) side <- rep(NA, n)
+  else {
+       if(n != length(side)) 
+          stop("Non-NULL argument 'side' should have the same length as x")
+       if(any(1 != abs(side[!is.na(side)]))) 
+          stop("Non-NULL argument 'side' should have values NA, +1, or -1.")
+       }
+
   case1or3 <- n == length(f)
+
   if((1 != length(f)) & !case1or3)
   	 stop("grad assumes a scalar valued function.")
+
   if(method=="simple"){
     #  very simple numerical approximation
     args <- list(eps=1e-4) # default
     args[names(method.args)] <- method.args
-    eps <- args$eps
+
+    side[is.na(side)] <- 1
+    eps <- rep(args$eps, n) * side
+
     if(case1or3) return((func(x+eps, ...)-f)/eps) 
+
     # now case 2
     df <- rep(NA,n)
     for (i in 1:n) {
       dx <- x
-      dx[i] <- dx[i] +eps 
-      df[i] <- (func(dx, ...)-f)/eps
+      dx[i] <- dx[i] + eps[i] 
+      df[i] <- (func(dx, ...) - f)/eps[i]
      }
     return(df)
-    } else
-  if(method=="complex"){ # Complex step gradient
+    } 
+  else if(method=="complex"){ # Complex step gradient
+    if (any(!is.na(side))) stop("method 'complex' does not support non-NULL argument 'side'.")
     eps <- .Machine$double.eps
     v <- try(func(x + eps * 1i, ...))
     if(inherits(v, "try-error")) 
@@ -51,11 +74,10 @@ grad.default <- function(func, x, method="Richardson",
       h0[i]  <- 0
       }
     return(g)
-    } else
-  if(method=="Richardson"){
+    } 
+  else if(method=="Richardson"){
     args <- list(eps=1e-4, d=0.0001, zero.tol=sqrt(.Machine$double.eps/7e-7), r=4, v=2, show.details=FALSE) # default
     args[names(method.args)] <- method.args
-    eps <- args$eps
     d <- args$d
     r <- args$r
     v <- args$v
@@ -67,17 +89,28 @@ grad.default <- function(func, x, method="Richardson",
     #  where the indexing variables k for rows(1 to r), i for columns (1 to n),
     #  r is the number of iterations, and n is the number of variables.
   
-    h <- abs(d*x)+eps*(abs(x) < args$zero.tol)
+
+    h <- abs(d*x) + args$eps * (abs(x) < args$zero.tol)
+    pna <- (side == 1)  & !is.na(side) # double these on plus side
+    mna <- (side == -1) & !is.na(side) # double these on minus side
+
     for(k in 1:r)  { # successively reduce h		    
-       if(case1or3)  a[k,] <- (func(x + h, ...) -  func(x - h, ...))/(2*h)
+       ph <- mh <- h
+       ph[pna] <- 2 * ph[pna] 
+       ph[mna] <- 0           
+       mh[mna] <- 2 * mh[mna] 
+       mh[pna] <- 0           
+
+       if(case1or3)  a[k,] <- (func(x + ph, ...) -  func(x - mh, ...))/(2*h)
        else for(i in 1:n)  {
-         if((k != 1) && (abs(a[(k-1),i]) < 1e-20)) a[k,i] <- 0 #some func are unstable near zero
-	 else  a[k,i] <- (func(x + h*(i==seq(n)), ...) - 
-	                  func(x - h*(i==seq(n)), ...))/(2*h[i])
-         }
+    	 if((k != 1) && (abs(a[(k-1),i]) < 1e-20)) a[k,i] <- 0 #some func are unstable near zero
+    	 else  a[k,i] <- (func(x + ph*(i==seq(n)), ...) - 
+    			  func(x - mh*(i==seq(n)), ...))/(2*h[i])
+    	 }
        if (any(is.na(a[k,]))) stop("function returns NA at ", h," distance from x.")
        h <- h/v     # Reduced h by 1/v.
        }	
+
    if(show.details)  {
         cat("\n","first order approximations", "\n")		
         print(a, 12)
@@ -117,27 +150,40 @@ grad.default <- function(func, x, method="Richardson",
 }
   
 
-jacobian <- function (func, x, method="Richardson",
+jacobian <- function (func, x, method="Richardson", side=NULL,
                               method.args=list(), ...) UseMethod("jacobian")
 
-jacobian.default <- function(func, x, method="Richardson",
+jacobian.default <- function(func, x, method="Richardson", side=NULL,
       method.args=list(), ...){
   f <- func(x, ...)
   n <- length(x)	 #number of variables.
+
+  if (is.null(side)) side <- rep(NA, n)
+  else {
+       if(n != length(side)) 
+          stop("Non-NULL argument 'side' should have the same length as x")
+       if(any(1 != abs(side[!is.na(side)]))) 
+          stop("Non-NULL argument 'side' should have values NA, +1, or -1.")
+       }
+
   if(method=="simple"){
     #  very simple numerical approximation
     args <- list(eps=1e-4) # default
     args[names(method.args)] <- method.args
-    eps <- args$eps
+
+    side[is.na(side)] <- 1
+    eps <- rep(args$eps, n) * side
+
     df <-matrix(NA, length(f), n)
     for (i in 1:n) {
       dx <- x
-      dx[i] <- dx[i] +eps 
-      df[,i] <- (func(dx, ...)-f)/eps
+      dx[i] <- dx[i] + eps[i] 
+      df[,i] <- (func(dx, ...) - f)/eps[i]
      }
     return(df)
-    } else
-  if(method=="complex"){ # Complex step gradient
+    } 
+  else if(method=="complex"){ # Complex step gradient
+    if (any(!is.na(side))) stop("method 'complex' does not support non-NULL argument 'side'.")
     # Complex step Jacobian
     eps <- .Machine$double.eps
     h0  <-  rep(0, n)
@@ -158,26 +204,35 @@ jacobian.default <- function(func, x, method="Richardson",
       h0[i]  <- 0
       }
     return(jac)
-    } else
-  if(method=="Richardson"){
+    } 
+  else if(method=="Richardson"){
     args <- list(eps=1e-4, d=0.0001, zero.tol=sqrt(.Machine$double.eps/7e-7), 
                 r=4, v=2, show.details=FALSE) # default
     args[names(method.args)] <- method.args
-    eps <- args$eps
     d <- args$d
     r <- args$r
     v <- args$v 	  
     a <- array(NA, c(length(f),r, n) )
   
-    h <- abs(d*x)+eps*(abs(x) < args$zero.tol)
-    for(k in 1:r)  { # successively reduce h		    
+    h <- abs(d*x) + args$eps * (abs(x) < args$zero.tol)
+    pna <- (side == 1)  & !is.na(side) # double these on plus side
+    mna <- (side == -1) & !is.na(side) # double these on minus side
+
+    for(k in 1:r)  { # successively reduce h		 
+       ph <- mh <- h
+       ph[pna] <- 2 * ph[pna] 
+       ph[mna] <- 0           
+       mh[mna] <- 2 * mh[mna] 
+       mh[pna] <- 0           
+
        for(i in 1:n)  {
-         a[,k,i] <- (func(x + h*(i==seq(n)), ...) -  
-	             func(x - h*(i==seq(n)), ...))/(2*h[i])
-         #if((k != 1)) a[,(abs(a[,(k-1),i]) < 1e-20)] <- 0 #some func are unstable near zero
-         }
+    	 a[,k,i] <- (func(x + ph*(i==seq(n)), ...) -  
+     		     func(x - mh*(i==seq(n)), ...))/(2*h[i])
+    	 #if((k != 1)) a[,(abs(a[,(k-1),i]) < 1e-20)] <- 0 #some func are unstable near zero
+    	 }
        h <- h/v     # Reduced h by 1/v.
-       }	
+       }     
+
    for(m in 1:(r - 1)) {	  
        a <- (a[,2:(r+1-m),,drop=FALSE]*(4^m)-a[,1:(r-m),,drop=FALSE])/(4^m-1)
      }

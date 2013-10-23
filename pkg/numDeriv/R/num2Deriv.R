0fc1344c5b818c82728e0e45d5610beb674351de
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2013-10-23 17:44:50 +0000

    changed p to n in code for consistency with grad and jacobian. Many minor formating changes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@788 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/numDeriv/R/num2Deriv.R
+++ pkg/numDeriv/R/num2Deriv.R
@@ -17,8 +17,9 @@ hessian.default <- function(func, x, method="Richardson",
    return(jacobian(func=function(fn, x, ...){grad(func=fn, x=x, 
            method="complex", method.args=list(eps=.Machine$double.eps), ...)}, 
          x=x, fn=func, method.args=args, ...))
-   } else 
- if(method != "Richardson")  stop("method not implemented.")
+   } 
+ else if(method != "Richardson")  stop("method not implemented.")
+
    args <- list(eps=1e-4, d=0.1, zero.tol=sqrt(.Machine$double.eps/7e-7), 
                 r=4, v=2, show.details=FALSE) # default
    args[names(method.args)] <- method.args
@@ -27,11 +28,12 @@ hessian.default <- function(func, x, method="Richardson",
    H <- diag(NA,length(x))
    u <- length(x)
    for(i in 1:length(x))
-     {for(j in 1:i) 
-        {u <- u + 1
+      for(j in 1:i){ 
+         u <- u + 1
          H[i,j] <- D[,u]
-     }  }
-   H <- H +t(H)
+         }
+
+   H <- H + t(H)
    diag(H) <- diag(H)/2
    H
   }
@@ -62,76 +64,65 @@ genD.default <- function(func, x, method="Richardson",
     args <- list(eps=1e-4, d=0.0001, zero.tol=sqrt(.Machine$double.eps/7e-7),
               r=4, v=2) # default
     args[names(method.args)] <- method.args
-    eps <- args$eps
     d <- args$d
     r <- args$r
     v <- args$v
     if (v!=2) stop("The current code assumes v is 2 (the default).")	 
-    #func.args <- list(...)
-     
-     #f0 <- do.call("func",append(list(x), func.args))
-     f0 <- func(x, ...)
-     #  f0 is the value of the function at x.
 
-     p <- length(x)  #  number of parameters (theta)
-     h0 <- abs(d*x)+eps*(abs(x) < args$zero.tol)
-     D <- matrix(0, length(f0),(p*(p + 3))/2)
-     #length(f0) is the dim of the sample space
-     #(p*(p + 3))/2 is the number of columns of matrix D.( first
-     #   der. & lower triangle of Hessian)
-     Daprox <- matrix(0, length(f0),r) 
-     Hdiag  <-  matrix(0,length(f0),p)
-     Haprox <-  matrix(0,length(f0),r)
-     for(i in 1:p)    # each parameter  - first deriv. & hessian diagonal
-  	  {h <-h0
-  	   for(k in 1:r)  # successively reduce h 
-  	     {f1 <- func(x+(i==(1:p))*h, ...)
-  	      f2 <- func(x-(i==(1:p))*h, ...) 
-  	      #f1 <- do.call("func",append(list(x+(i==(1:p))*h), func.args))
-  	      #f2 <- do.call("func",append(list(x-(i==(1:p))*h), func.args))
-  	      Daprox[,k] <- (f1 - f2)  / (2*h[i])    # F'(i) 
-  	      Haprox[,k] <- (f1-2*f0+f2)/ h[i]^2     # F''(i,i) hessian diagonal
-  	      h <- h/v     # Reduced h by 1/v.
-  	      NULL
-  	     }
-  	   for(m in 1:(r - 1))
-  	      for ( k in 1:(r-m))
-  		{Daprox[,k]<-(Daprox[,k+1]*(4^m)-Daprox[,k])/(4^m-1)
-  		 Haprox[,k]<-(Haprox[,k+1]*(4^m)-Haprox[,k])/(4^m-1)
-  		 NULL
-  		}
-  	   D[,i] <- Daprox[,1]
-  	   Hdiag[,i] <- Haprox[,1]
-  	   NULL
-  	  }	  
-     u <- p
+    f0 <- func(x, ...)   #f0 is the value of the function at x.
+
+    n <- length(x)  #  number of parameters (theta)
+    h0 <- abs(d*x) + args$eps * (abs(x) < args$zero.tol)
+    D <- matrix(0, length(f0),(n*(n + 3))/2)
+    #length(f0) is the dim of the sample space
+    #(n*(n + 3))/2 is the number of columns of matrix D.( first
+    #	der. & lower triangle of Hessian)
+    Daprox <-  matrix(0,length(f0),r) 
+    Hdiag  <-  matrix(0,length(f0),n)
+    Haprox <-  matrix(0,length(f0),r)
+
+    for(i in 1:n){    # each parameter  - first deriv. & hessian diagonal
+          h <-h0
+          for(k in 1:r){  # successively reduce h 
+             f1 <- func(x+(i==(1:n))*h, ...)
+             f2 <- func(x-(i==(1:n))*h, ...) 
+             #f1 <- do.call("func",append(list(x+(i==(1:n))*h), func.args))
+             #f2 <- do.call("func",append(list(x-(i==(1:n))*h), func.args))
+             Daprox[,k] <- (f1 - f2)  / (2*h[i])    # F'(i) 
+             Haprox[,k] <- (f1-2*f0+f2)/ h[i]^2     # F''(i,i) hessian diagonal
+             h <- h/v	  # Reduced h by 1/v.
+             }
+          for(m in 1:(r - 1))
+             for ( k in 1:(r-m)){
+                Daprox[,k]<-(Daprox[,k+1]*(4^m)-Daprox[,k])/(4^m-1)
+        	Haprox[,k]<-(Haprox[,k+1]*(4^m)-Haprox[,k])/(4^m-1)
+                }
+          D[,i] <- Daprox[,1]
+          Hdiag[,i] <- Haprox[,1]
+          }	 
+    u <- n
   
-     for(i in 1:p)   # 2nd derivative  - do lower half of hessian only
-       {for(j in 1:i) 
-  	  {u <- u + 1
-  	      if (i==j) { D[,u] <- Hdiag[,i]; NULL}
-  	      else 
-  	       {h <-h0
-  		for(k in 1:r)  # successively reduce h 
-  		  {f1 <- func(x+(i==(1:p))*h + (j==(1:p))*h, ...)
-  		   f2 <- func(x-(i==(1:p))*h - (j==(1:p))*h, ...)
-  		   #f1 <- do.call("func", append(
-  		   #	  list(x+(i==(1:p))*h + (j==(1:p))*h), func.args))
-  		   #f2 <- do.call("func",append(
-  		   #	  list(x-(i==(1:p))*h - (j==(1:p))*h), func.args))  
-  		   Daprox[,k]<- (f1 - 2*f0 + f2 -
-  				    Hdiag[,i]*h[i]^2 - 
-  				    Hdiag[,j]*h[j]^2)/(2*h[i]*h[j])  # F''(i,j)  
-  		   h <- h/v	# Reduced h by 1/v.
-  		  }
-  		for(m in 1:(r - 1))
-  		   for ( k in 1:(r-m))
-  		     {Daprox[,k]<-(Daprox[,k+1]*(4^m)-Daprox[,k])/(4^m-1); NULL}
-  		D[,u] <- Daprox[,1]
-  		NULL
-  	       }
-  	    }  
-  	 }
+    for(i in 1:n){   # 2nd derivative  - do lower half of hessian only
+       for(j in 1:i){
+          u <- u + 1
+             if (i==j) D[,u] <- Hdiag[,i]
+             else {
+               h <-h0
+               for(k in 1:r){  # successively reduce h 
+        	  f1 <- func(x+(i==(1:n))*h + (j==(1:n))*h, ...)
+        	  f2 <- func(x-(i==(1:n))*h - (j==(1:n))*h, ...)
+        	  Daprox[,k]<- (f1 - 2*f0 + f2 -
+        			   Hdiag[,i]*h[i]^2 - 
+        			   Hdiag[,j]*h[j]^2)/(2*h[i]*h[j])  # F''(i,j)  
+        	  h <- h/v     # Reduced h by 1/v.
+        	  }
+               for(m in 1:(r - 1))
+        	  for ( k in 1:(r-m))
+        	     Daprox[,k]<-(Daprox[,k+1]*(4^m)-Daprox[,k])/(4^m-1)
+               D[,u] <- Daprox[,1]
+               }
+          }  
+       }
   D <- list(D=D, p=length(x), f0=f0, func=func, x=x, d=d,
             method=method, method.args=args)# Darray constructor (genD.default)
   class(D) <- "Darray"

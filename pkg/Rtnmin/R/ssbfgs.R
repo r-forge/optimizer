3cc06724ff79a0b34f18123954d069aba40ac0e0
nashjc (Prof ) <nashjc@uottawa.ca>
2014-06-03 15:22:44 +0000

    remove functions in Rtnmin.package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@819 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rtnmin/R/ssbfgs.R
+++
@@ -1,9 +0,0 @@
-ssbfgs <- function (s, hv, hy, ys, yhy, vs, vhy){
-##---------------------------------------------------------
-## self-scaled bfgs quasi-Newton update 
-## (used by preconditioner)
-##---------------------------------------------------------
-  delta <- (1 + yhy/ys)*vs/ys - vhy/ys
-  beta  <- -vs/ys
-  z     <- hv + delta*s + beta*hy
-}
--- pkg/Rtnmin/R/ztime.R
+++
@@ -1,9 +0,0 @@
-ztime<-function(x,ipivot) {
-   ## ---------------------------------------------------------
-   ##  this routine multiplies the vector x by the 
-   ##  constraint matrix z
-   ## ---------------------------------------------------------
-   ind <- which(ipivot!=0);
-   x[ind] <- 0
-   x1 <- x;
-}

e568d13cc9cb2ab850efc8b8e5f633ef8ab90bec
nashjc (Prof ) <nashjc@uottawa.ca>
2015-04-28 00:39:55 +0000

    remove optplus
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@949 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optplus/R/grnd.R
+++
@@ -1,8 +0,0 @@
-############### grnd.R ####################
-## require("numDeriv")
-grnd<-function(par, userfn, ...) { # using grad from numDeriv
-# with Richardson method
-   tryg<-grad(userfn, par, ...)
-}
-############### end grnd ####################
-

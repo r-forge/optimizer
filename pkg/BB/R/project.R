38e6d8c9d669fb32279e4fa172c6bc04d6544cde
pgilbert (Paul Gilbert) <pgilbert.ttv9z@ncf.ca>
2015-12-03 19:32:28 +0000

    replacing an experimental, broken, version of project.R that was accidentally committed. This revision now tests and builds ok, but outstanding issue with special version of projection is not yet resolved.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1039 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BB/R/project.R
+++ pkg/BB/R/project.R
@@ -1,71 +1,71 @@
+
 projectLinear <- function(par, A, b, meq) {
-# A projection function to incorporate linear equalities and 
-# inequalities in nonlinear optimization using `spg'
+# A projection function to incorporate linear equalities and inequalities in nonlinear optimization using `spg'
 #
-# The inequalities are defined such that:  A %*% x - b > 0
+# The inequalities are defined such that:  A %*% x - b > 0 
 
-   # local function
-   solve.QP <- function(dvec, Amat, bvec, meq=0, EPS=1e-07){
-     sol <- dvec
-     imeq <- seq_len(meq)
-
-     Nmat <- NULL
-     wvec <- NULL
-     active <- NULL
-
-     repeat{
-       viol <-  crossprod(Amat, sol) - bvec
-       viol1 <- viol / pmax(1,abs(bvec))
-       ## viol1 <- viol / (1 + abs(bvec))
-
-       iim <- viol1[imeq] >= EPS
-       if( any(iim) ){
-   	iim <- which(iim)
-   	viol[iim] <- -viol[iim]
-   	bvec[iim] <- -bvec[iim]
-   	Amat[,iim] <- -Amat[,iim]
-       }
-
-       ii <- which.min(viol1)[1]
-       if( viol1[ii] > -EPS) break
-
-       if(ii %in% active)
-   	stop("Error in projection")
-
-       wvec <- c(wvec, 0)
-       active <- c(active, ii)
-       npvec <- Amat[,ii]
-       if( !is.null(Nmat) ){
-   	rvec <- solve(qr(Nmat, LAPACK=TRUE), npvec)
-   	dvec <- npvec - c(Nmat %*% rvec)
-       }else{
-   	dvec <- npvec
-   	rvec <- NULL
-       }
-
-       jj <- rvec > 0
-       jj[1] <- FALSE
-
-       tmp <- wvec[jj]/rvec[jj]
-       t1 <- suppressWarnings(min(tmp))
-       t2 <- -viol[ii]/crossprod(npvec, dvec)
-
-       t <- min(c(t1, t2))
-       if( !is.finite(t) || t < 0 || t1 <= t2 )
-   	stop("Error in projection")
-
-       sol <- sol + t * dvec
-       wvec <- wvec - t * c(rvec, -1)
-       Nmat <- cbind(Nmat, npvec)
-     }
-     return(c(sol))
-   }
+## This special version of solve.QP works often but fails sometimes.
+## Had to revert to quadprog version, October 2014.
+##   # local function
+##   solve.QP <- function(dvec, Amat, bvec, meq=0){
+##     EPS=1e-07
+##     sol <- dvec
+##     imeq <- seq_len(meq)
+##     
+##     Nmat <- NULL
+##     wvec <- NULL
+##     active <- NULL
+## 
+##     repeat{
+## 	 viol <- crossprod(Amat, sol) - bvec
+## 	 iim <- viol[imeq] >= 0
+## 	 if( any(iim) ){
+## 	   iim <- which(iim)
+## 	   viol[iim] <- -viol[iim]
+## 	   bvec[iim] <- -bvec[iim]
+## 	   Amat[,iim] <- -Amat[,iim]
+## 	 }
+## 	    
+## 	 ii <- which.min(viol)[1]
+## 	 if( viol[ii] > -EPS) break
+## 
+## 	 if(ii %in% active)
+## 	   stop("Error in projection")
+## 	 
+## 	 wvec <- c(wvec, 0)
+## 	 active <- c(active, ii)
+## 	 npvec <- Amat[,ii]
+## 	 if( !is.null(Nmat) ){
+## 	   rvec <- solve(qr(Nmat, LAPACK=TRUE), npvec)
+## 	   dvec <- npvec - c(Nmat %*% rvec)
+## 	 }else{
+## 	   dvec <- npvec
+## 	   rvec <- NULL
+## 	 }
+## 	 
+## 	 jj <- rvec > 0
+## 	 jj[1] <- FALSE
+## 	 
+## 	 tmp <- wvec[jj]/rvec[jj]
+## 	 t1 <- suppressWarnings(min(tmp))	 
+## 	 t2 <- -viol[ii]/crossprod(npvec, dvec)
+## 	 
+## 	 t <- min(c(t1, t2))
+## 	 if( !is.finite(t) || t < 0 || t1 <= t2 )
+## 	   stop("Error in projection")
+## 	 
+## 	 sol <- sol + t * dvec
+## 	 wvec <- wvec - t * c(rvec, -1)
+## 	 Nmat <- cbind(Nmat, npvec)
+##     }
+##     return(c(sol))
+##   }
 
 n <- length(par)
 if (meq > 0 | any(b - c(A %*% par) > 0)){
-    ans <-solve.QP(dvec=rep(0, n), Amat=t(A),
-                       bvec = b - c(A %*% par), meq=meq, EPS=1e-07)
-    par <- par + ans
+   par <- par +  quadprog::solve.QP(Dmat=diag(1,n), dvec=rep(0, n), Amat=t(A),
+		      bvec = b - c(A %*% par), meq=meq, factorized=TRUE)$solution
 }
 par
 }
+

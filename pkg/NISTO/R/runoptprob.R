ff1b3e4f32dec6569261608cafaf4d009b44a065
nashjc (Prof ) <nashjc@uottawa.ca>
2016-11-14 21:32:58 +0000

    getting runoptprob running 1st time on tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NISTO/R/runoptprob.R
+++ pkg/NISTO/R/runoptprob.R
@@ -1,17 +1,26 @@
 ## @knitr runoptprob.R
 
-runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL, 
-                       nstart = 0, options=list(args=NULL, control=NULL) ) {
+runoptprob <- function(pfilename, minmeth='nls', submeth=NULL, 
+                       nstart = 0, runopts=list(args=NULL, control=NULL) ) {
+
   #- ?? Need to eval(parse()) ALL functions available, since f calls res etc.
   #- Need to carefully ensure these exist to avoid errors??
   #- ?? can we simplify and NOT have to eval(parse()) them, but simply source the prb file?
   
-  print(options$args)
-  print(options$controls)
+  print(runopts$args)
+  print(runopts$controls)
   optecho <- TRUE # temporarily at least, or put in a profile
   #- Get the path to the files (where should these be? Probably somehow related to pkg)   
   pfile <- paste(pfilename, ".prb", sep='')
-  source(pfile, echo=optecho) # -- filename (at least the root)
+
+  starts <- NA
+  mformula <- NA # Make sure these are defined (they get set up in pfile)
+#- ?? need to figure out dynamic setting of paths
+  pfilepath <- paste("/home/john/rsvnall/optimizer/pkg/NISTO/inst/extdata/",pfile,sep='')
+  source(pfilepath, echo=optecho) # -- filename (at least the root)
+  cat("Objects in workspace:\n")
+  print(ls())
+
   #- now have a lot of the information
   
   #-    -- which tool to use (optimr, nls, nlmrt, nlsr tools, minpack.LM tools)
@@ -34,11 +43,15 @@ runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL,
   
   #-  - read the file and execute it (make sure it has **R** commands so we can
   #-   actually source() it)
-  #- ?? not needed eval(parse(text=paste(pfilename,".setup", sep=''))) #- setup function
+  setupfn <- eval(parse(text=paste(pfilename,".setup", sep=''))) #- setup function
+  pdat <- setupfn()
+  cat("pdat:\n")
+  print(pdat)
   #- get the data and the starts
-  dfname <- eval(parse(text=paste(pfilename,".df", sep='')))
-  print(dfname)
-  
+  print(pdat$df)
+  dfname <- pdat$df
+  starts <- pdat$starts
+  mformula <- pdat$mformula ##?? later on simplify and remove extra lines 
   #- - analyze the call to runprob and do the appropriate call
   
   #- - format output and extract and store summaries
@@ -49,7 +62,9 @@ runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL,
   
   #-  -- need to save conditions
   if (nstart == 0) { # need to loop
-    nst <- seq(1: dim(starts)[1])
+    cat("nstart == 0 \n")
+    print(pdat$starts)
+    nst <- seq(1, (dim(pdat$starts)[1]))
   } else { nst <- nstart }
   if (minmeth == 'nls') {
     for (istrt in nst){
@@ -61,7 +76,7 @@ runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL,
     }    
   }  
   if (minmeth == 'nlxb') {
-      require(nlmrt)
+##??      require(nlmrt)
       for (istrt in nst){
       #- ?? need to extract options and arguments like trace
       #- some documentation output needed ??
@@ -71,16 +86,16 @@ runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL,
     }    
   }  
   if (minmeth == "optimr") {
-     require(optimrx) #- ?? optimr for CRAN
+## ??     require(optimrx) #- ?? optimr for CRAN
     #- here need to check if they exist??
      ufn <- eval(parse(text=paste(pfilename,".f", sep='')))
      eval(parse(text=paste(pfilename,".res", sep='')))
      eval(parse(text=paste(pfilename,".jac", sep='')))
      eval(parse(text=paste(pfilename,".g", sep='')))
-     if( is.null(options$args[["gr"]]) || ! is.character(options$args[["gr"]]) ) {
+     if( is.null(runopts$args[["gr"]]) || ! is.character(runopts$args[["gr"]]) ) {
        #- name.gr now a function      
        ugr <- eval(parse(text=paste(pfilename,".g", sep='')))
-     } else { ugr <- (options$args[["gr"]]) }
+     } else { ugr <- (runopts$args[["gr"]]) }
      ufn <-  eval(parse(text=paste(pfilename,".f", sep='')))
      for (istrt in nst){
         sol <- optimr(starts[istrt,], ufn, ugr, method=submeth, control=list(trace=1))
@@ -90,4 +105,5 @@ runoptprob <- function(pfilename=NULL, minmeth='nls', submeth=NULL,
     #- result should be a list of things run
   #- ?? sink() # should make conditional and not usually do this, else delete after asking
   testsol <- list(pfilename=pfilename, nstart=nstart, minmeth=minmeth, submeth=submeth)
+#- ?? definitely need more stuff returned
 }

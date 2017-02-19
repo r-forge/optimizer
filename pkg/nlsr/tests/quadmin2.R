b64cb554abdfdb40a16378f22334e28abc3c48c2
nashjc (Prof ) <nashjc@uottawa.ca>
2017-02-19 16:14:29 +0000

    Fixed http https issue in a vignette Added tentative example test of Newton method on quadratic.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1397 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nlsr/tests/quadmin2.R
@@ -0,0 +1,30 @@
+# Test of nlsr hessian and gradient
+
+fexp <- ~ (x-1)^2 + (y-3)^2
+fexp
+library(nlsr)
+myder <- fnDeriv(fexp, c("x","y"), hessian=TRUE)
+myder
+strt <- c(x=0, y=-0)
+astrt <- myder(strt)
+strt <- c(x=0, y=0)
+myd <- function(prm){
+  x <- prm[1]
+  y <- prm[2]
+  val <- myder(x, y)
+}
+astrt <- myd(strt)
+astrt
+hm1 <- as.matrix(attr(astrt, "hessian"))
+hm1
+hm <- matrix(hm1, nrow=2)
+hm
+g1 <- attr(astrt, "gradient")
+g1 <- as.vector(g1)
+g1
+delta <- solve(hm, g1)
+delta
+new <- strt-delta
+new
+newf<-myd(new)
+newf

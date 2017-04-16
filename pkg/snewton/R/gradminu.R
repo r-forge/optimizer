3ffa5fa50bc3211c6284e8bf81f809f0303e5805
nashjc (Prof ) <nashjc@uottawa.ca>
2017-04-16 21:50:22 +0000

    try to add gradmin to snewton
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1449 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/snewton/R/gradminu.R
+++ pkg/snewton/R/gradminu.R
@@ -219,28 +219,3 @@ if (ws$lsmeth == "default") {
   out$niter<-niter
   out 
 }
-
-# Try testing calls to see what is transferred (eventually test also ...)
-# setup
-x0<-c(1,2,3,4)
-fnt <- function(x, fscale=10){
-  yy <- length(x):1
-  val <- sum((yy*x)^2)*fscale
-}
-grt <- function(x, fscale=10){
-  nn <- length(x)
-  yy <- nn:1
-  #    gg <- rep(NA,nn)
-  gg <- 2*(yy^2)*x*fscale
-  gg
-}
-
-hesst <- function(x, fscale=10){
-  nn <- length(x)
-  yy <- nn:1
-  hh <- diag(2*yy^2*fscale)
-  hh
-}
-
-t1 <- gradminu(x0, fnt, grt, hesst, control=list(trace=2), fscale=3.0)
-t1

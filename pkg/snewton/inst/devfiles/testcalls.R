4e38dc165d0d64429c99f0c7dbdffb14aff23998
nashjc (Prof ) <nashjc@uottawa.ca>
2017-04-16 15:43:53 +0000

    Sorted testcalls using simple list that is passed as an argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1445 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/snewton/inst/devfiles/testcalls.R
+++ pkg/snewton/inst/devfiles/testcalls.R
@@ -22,6 +22,46 @@ hesst <- function(x, fscale=10){
 
 # Here want to set up ways to access other functions that are called inside testcalls
 
+Newtdir <- function(x, fn, gr, hess, wspace, control=list(), ...) {
+ # wspace <- control$wrk # This is more certain, but not much more effort
+#  wrkenv <- parent.env(environment()) # the env of calling routine
+  cat("Newtdir -- wspace:")
+  print(wspace)
+  cat("Newtdir -- fn, gr, hess:")
+  print(fn)
+  print(gr)
+  print(hess)
+  cat("Newtdir -- wspace$gcurr:")
+  print(wspace$gcurr)
+#  cat('exists("wspace$gcurr") =', exists("wspace$gcurr"),"\n")
+  # seems exists is only for the NAME
+  cat("is.null('wspace$gcurr') =", is.null('wspace$gcurr'),"\n")
+  # Try to compute Newton direction
+  if (is.null(wspace$gcurr)) {
+     cat("Compute gradient gcurr in Newtdir\n")
+     gcurr <- gr(x, ...)
+     wspace$gcurr <- gcurr
+  } else {
+    cat("gcurr from wspace:")
+    gcurr <- wspace$gcurr
+    print(gcurr)
+  }
+  # Need some way to pass this back to the calling environment
+  if (is.null(wspace$hcurr)) {
+    hcurr <- hess(x, ...) # ditto
+    wspace$hcurr <- hcurr
+    cat("generated hcurr:")
+    print(hcurr)
+  } else { hcurr <- wspace$hcurr }
+  cat("Newtdir, hcurr and gcurr\n")
+  print(hcurr)
+  print(gcurr)
+  sdir <- solve(hcurr, -gcurr)
+  attr(sdir,"wspace") <- wspace
+  print(sdir)
+  sdir
+}
+  
 
 
 testcalls<-function(x0, fn, gr, hess, lower = NULL, upper = NULL, 
@@ -80,7 +120,38 @@ for (onename in nctrld) {
   }
 }
 
+wspace <- list() #
 
-  out 
+cat("generate gcurr in testcalls\n")
+x <- x0
+wspace$gcurr <- gr(x0, ...)
+print(wspace$gcurr)
+
+sd1 <- Newtdir(x, fn, gr, hess, wspace, control, ...)
+cat("sd1:")
+print(sd1)
+
+cat("Now NULL gcurr in wspace\n")
+wspace$gcurr <- NULL
+
+sd2 <- Newtdir(x, fn, gr, hess,  wspace, control, ...)
+cat("sd2:")
+print(sd2)
+cat("Present gcurr:")
+print(gcurr)
+
+out <- sd2
+out
 }
+
+cat("Testcalls -- 170415 -- to illustrate how R transfers information\n")
+
+res10 <- testcalls(x0, fnt, grt, hesst, fscale=10.0)
+cat("res10:   ")
+print(res10)
+
+cat("\nNow try with fscale=1\n")
+res1 <- testcalls(x0, fnt, grt, hesst, fscale=1.0)
+cat("res1:    ")
+print(res1)
 

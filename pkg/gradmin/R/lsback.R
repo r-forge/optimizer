cd13f9ef95fae8af133dc208fd8e786ccd50f6fd
nashjc (Prof ) <nashjc@uottawa.ca>
2017-04-17 13:38:20 +0000

    will have to commit then delete. Sigh.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1451 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gradmin/R/lsback.R
@@ -0,0 +1,22 @@
+lsback<-function(fn, fbest, xc, d, grv, ws, ...) { 
+  # lsback -- backtrack line search
+  st <- 1.0
+  gproj <- as.numeric(crossprod(grv, xc) )
+  repeat {
+    xnew <- xc + st*d # new point
+    if ((ws$offset+xnew) == (ws$offset+xc)) { # no better parameters
+      st <- 0
+      rlout <- st
+      attr(rlout,"Fval")<-fbest # Assume we pass this in
+      return(rlout)
+    }
+    fval <- fn(xnew, ...)
+    ws$nf <- ws$nf + 1
+    if (ws$trace > 1) cat("Step = ",st," fval = ",fval,"\n")
+    if (fval <= fbest + ws$acctol*st*gproj) break
+    st <- ws$stepdec*st # new step
+  }
+  rlout <- st
+  attr(rlout, "Fval")<- fval
+  rlout
+} # end backtrack line search
---
+++ pkg/gradmin/tests/gmsimple.R
@@ -0,0 +1,26 @@
+
+# Try testing calls to see what is transferred (eventually test also ...)
+# setup
+x0<-c(1,2,3,4)
+fnt <- function(x, fscale=10){
+  yy <- length(x):1
+  val <- sum((yy*x)^2)*fscale
+}
+grt <- function(x, fscale=10){
+  nn <- length(x)
+  yy <- nn:1
+  #    gg <- rep(NA,nn)
+  gg <- 2*(yy^2)*x*fscale
+  gg
+}
+
+hesst <- function(x, fscale=10){
+  nn <- length(x)
+  yy <- nn:1
+  hh <- diag(2*yy^2*fscale)
+  hh
+}
+
+library(snewton)
+t1 <- gradminu(x0, fnt, grt, hesst, control=list(trace=2), fscale=3.0)
+t1

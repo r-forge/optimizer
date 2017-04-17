730d2fc5233cc51973baa8a5798644d0ace9ec5b
nashjc (Prof ) <nashjc@uottawa.ca>
2017-04-17 13:38:46 +0000

    remove again
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1452 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gradmin/R/lsnone.R
+++
@@ -1,8 +0,0 @@
-lsnone<-function(fn, fbest, xc, d, grv, ws, ...) { 
-  # lsnone -- ALWAYS returns unit stop
-  # ?? count fevals?
-  rlout <- 1.0
-  ws$nf <- ws$nf+1
-  attr(rlout, "Fval")<- fn(xc + d)
-  rlout
-} # end lsnone

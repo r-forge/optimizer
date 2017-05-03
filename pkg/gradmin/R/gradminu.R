6d1d929337b342106822130cd7bf23e286fb7814
nashjc (Prof ) <nashjc@uottawa.ca>
2017-05-03 22:25:38 +0000

    switching over to use of the workspace entirely
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1504 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gradmin/R/gradminu.R
+++ pkg/gradmin/R/gradminu.R
@@ -7,13 +7,15 @@ gradminu<-function(x0, fn, gr, hess, lower = NULL, upper = NULL,
 ##?? fixup documentation here??
 ##       - x0 is the initial value
 ##       - ... is data used in the function fn
-##Output (list) -- need to match optim() output!! ???
-##       - xs is the value at the minimum
-##       - fv is the fn evaluated at xs
-##       - grd is the gradient
-##       - Hess is the Hessian
-##       - niter is the number of interations needed (gradient and Hessian evals).
-##       - add fevals??, other reports
+##Output (list) -- matching optimr() output
+#  par	  The best set of parameters found.
+#  value	The value of ‘fn’ corresponding to ‘par’.
+#  counts	A 3-element integer vector giving the number of calls to 
+#      ‘fn’ and ‘gr’ and 'hess' respectively. 
+#  convergence	An integer code. ‘0’ indicates successful completion
+#  message	 A character string giving any additional information 
+#  returned by the optimizer, or ‘NULL’.
+#  hessian	Either NULL or the Hessian matrix at the parameters par.
 
 # Need a way to pass any object defined at this level into routines called
 # But not necessarily to define them WITHIN this routine
@@ -29,30 +31,27 @@ terminate <- function(w, ...){
     if (w$trace > 0) cat("Too many (",niter," iterations\n")
     halt <- TRUE
     w$convcode <- 1
-    
+    break    
   }
   if (w$nf >= w$maxfevals) {
-    cat("Stopping\n")
-    if (w$trace > 0) cat("Too many ",w$nf," function evaluations\n")
+    w$msg <- paste("Too many (",w$nf,") function evaluations")
+    if (w$trace > 0) cat(w$msg,"\n")
     halt <- TRUE
-    convcode <- 91 # ?? value
+    w$convcode <- 1 # ?? value
     break
   }
   #    if (w$ng > w$maxgevals){} # not implemented
   #    if (w$nh > w$maxhevals){} # not implemented
-  gmax <- max(abs(grd))
-  if (gmax <= w$epstol*f0) {
-    if (w$trace > 0) cat("Small gradient norm",gmax,"\n")
+  gmax <- max(abs(w$grd))
+  if (gmax <= w$epstol*w$f0) {
+    w$msg <- paste("Small gradient norm",gmax)
+    if (w$trace > 0) cat(w$msg,"\n")
     halt <- TRUE
     w$convcode <- 0 # OK
   }
-  
-  
-  
+  halt
 }  
   
-  
-      
 if (control$trace > 2) {
     cat("control:")
     print(control)
@@ -90,6 +89,7 @@ ctrl <- list(
   f0 = NA,
   stopiter = FALSE,
   convcode = NA,
+  msg = "??",
   lastng = 0
 )  
 
@@ -102,84 +102,69 @@ for (onename in ncontrol) {
 }
 
 w <- list2env(ctrl) # Workspace
+w$fn <- fn
+w$gr <- gr
+w$hess <- hess
 
-#  w$lambda<-w$lamstart ## ?? do better
+
+  w$lambda<-w$lamstart ## ?? do better
+  w$lastng <- 1 # for VM methods "last gradient"
   w$niter <- 1
   w$xb <- x0 # best so far
   w$fbest <- fn(w$xb, ...)
   w$nf <- w$nf + 1
   if ((class(w$fbest) == "try-error") | is.na(w$fbest) | 
                  is.null(w$fbest) | is.infinite(w$fbest)) {
-    msg <- "Initial point gives inadmissible function value"
-    conv <- 20 # ?? choice of settings
+    w$msg <- "Initial point gives inadmissible function value"
+    w$convcode <- 20 # ?? choice of settings
     if (trace > 0)  cat(msg, "\n") 
-    out <- list(xb, w$bigval, c(w$nf, 0), conv, msg)  #
+    out <- list(w$xb, w$bigval, c(w$nf, 0, 0), w$convcode, w$msg)  #
     names(out) <- c("par", "value", "counts", "convergence", 
                     "message")
 # ?? add Hess etc??
     return(out)
   }
   
-  if (w$trace > 1) cat("Initial Fval =",fbest,"\n")
-  w$f0 <- fbest # for scaling
-#  cat("w$nf now ",w$nf,"\n")
+  if (w$trace > 1) cat("Initial Fval =",w$fbest,"\n")
+  w$f0 <- w$fbest # for scaling
   # Initialize
-  start <- w$minmeth(w, msetup=TRUE)
-  if (! start) stop(paste("Could not commence method ","name of minmeth","\n"))
+  if (w$minmeth(w, msetup=TRUE) != 0) 
+       stop(paste("Could not commence method ",deparse(substitute(w$minmeth)),"\n"))
+  cat("returned w$grd:")
+  print(w$grd)
   repeat {
-        if (w$trace > 0) {cat("Iteration ",niter,"  Fval=",fbest,"\n")}
-##    }
-    stp<-try(w$solver(H, -grd))
-    if (class(stp) == "class-error") {
-          stop("Failure of default solve of Newton equations")
-    }
-##    } else if (w$solver == "marquardt") {
-##       Haug<-H + (diag(H)+1.0)*lambda # To avoid singularity
-##       stp <- solve(Haug, -grd)
-    
-    if (w$trace > 1) {
+        if (w$trace > 0) {cat("Iteration ",w$niter,"  Fval=",w$fbest,"\n")}
+     stp <- w$minmeth(w) # no msetup
+     if (w$trace > 1) {
          cat("Search vector:")
          print(stp)
-    }
-
-    gprj <- as.numeric(crossprod(stp, grd))
-    if (w$trace > 1) cat("Gradient projection = ",gprj)
-##    tmp <- readline("   continue?")
-    ## Do line search
-    gvl<-w$lnsrch(fn,fbest, xb, stp, grv=grd, w, ...)
-    if (attr(gvl, "FAIL")) {
+     }
+     if (w$trace > 1) {
+        gprj <- as.numeric(crossprod(stp, w$grd))
+        cat("Gradient projection = ",gprj)
+     }
+     ## Do line search
+     w$gvl<-w$lnsrch(w, ...)
+     if (attr(gvl, "FAIL")) {
         if ((w$trace > 0) && (w$trace > 0)) cat("Line search fails\n")
         break
-    }
-    fval <- attr(gvl,"Fval")
+     }
+     fval <- attr(gvl,"Fval")
 ##    cat("after lnsrch w$nf now ",w$nf,"\n")
-    if (w$trace > 2) {cat(" step =", gvl,"  fval=",fval ," w$nf=",w$nf,"\n")}
-    xn<-xb+gvl*stp
+    if (w$trace > 2) {cat(" step =", w$gvl,"  fval=",fval ," w$nf=",w$nf,"\n")}
+    xn<-xb+w$gvl*stp
     if (niter >= w$maxit) {
       print("NewtonR: Failed to converge!")
       return(0)
     }
-##    if (w$solver == "marquardt"){
-##       if (fval <= fbest) {
-##          xb <- xn
-##          fbest <- fval
-##          lambda <- lambda * w$lamdec
-##          getH <- TRUE # ensure we start over
-##       } else {
-##          getH <- FALSE # don't want new H, grd
-##          lambda <- lambda * w*laminc
-##       }
-##    }
     xb <- xn
     fbest <- fval
     niter <- niter + 1
     if (w$watch) tmp <- readline("end iteration")   
   } # end repeat
   out<-NULL # ensure cleared first, and then use structure above
-  out$xs<-xn
-  out$fv<-fval
-  out$grd<-grd
-  out$Hess<-H # ?? w$H
-  out$niter<-niter
-  out 
-}
+  out <- list(w$xb, w$fbest, c(w$nf, w$ng, w$nh), w$convcode, w$msg, w$H)  #
+  names(out) <- c("par", "value", "counts", "convergence", 
+                  "message", "hessian")
+  out
+  }
--- pkg/gradmin/R/lsback.R
+++ pkg/gradmin/R/lsback.R
@@ -1,14 +1,14 @@
-lsback<-function(fn, fbest, xc, d, grv, w, ...) { 
+lsback<-function(w, ...) { 
   # lsback -- backtrack line search
   st <- 1.0
-  gproj <- as.numeric(crossprod(grv, xc) )
+  w$gproj <- as.numeric(crossprod(w$grd, w$tdir) ) # can we save this from elsewhere?
   repeat {
-    xnew <- xc + st*d # new point
-    if (all((w$offset+xnew) == (w$offset+xc))) { # no better parameters
+    xnew <- w$xb + st*w$tdir # new point
+    if (all((w$offset+xnew) == (w$offset+w$xc))) { # no better parameters
       if (w$trace > 1) cat("No progress in lsback\n")
       st <- 0
       rlout <- st
-      attr(rlout,"Fval")<-fbest # Assume we pass this in
+      attr(rlout,"Fval")<-w$fbest # Assume we pass this in
       attr(rlout,"FAIL")<- TRUE
       return(rlout)
     }
@@ -16,11 +16,11 @@ lsback<-function(fn, fbest, xc, d, grv, w, ...) {
     w$nf <- w$nf + 1
     if (w$trace > 2) cat("Step = ",st," fval = ",fval,"\n")
     if ((w$trace > 1) && (w$watch)) tmp <- readline("lnsrch continue?")
-    if (fval <= fbest + w$acctol*st*gproj) break # Armijo condition
+    if (fval <= w$fbest + w$acctol*st*gproj) break # Armijo condition
     st <- w$stepdec*st # new step
   }
   rlout <- st
-  attr(rlout, "Fval")<- fval
+  attr(rlout, "Fval")<- fval # could return through w
   attr(rlout,"FAIL")<- FALSE
   rlout
 } # end backtrack line search

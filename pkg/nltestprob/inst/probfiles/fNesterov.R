74c4b4ab495398d04b132db6bde94653fd6d82fc
nashjc (Prof ) <nashjc@uottawa.ca>
2017-09-20 20:39:57 +0000

    fixup Nesterov, template
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1569 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nltestprob/inst/probfiles/fNesterov.R
+++ pkg/nltestprob/inst/probfiles/fNesterov.R
@@ -1,4 +1,36 @@
-fnNest<-function(x){
+## @knitr Nesterov.prb
+# This is file Nesterov.prb
+probname <- "Nesterov"
+probdesc <- "Put your description in double quotes.
+"
+
+#- Note: environment / list "pe" must already exist
+
+if (! exists("pe")) {stop("Environment 'pe' must exist before running problems.")}
+
+#- nls format expression
+Nesterov.formula <- ( y ~ b1*x**b2 )
+
+#- setup
+# Nesterov.setup<-function() {
+y <- c( 2.138, 3.421, 3.597, 4.340, 4.882, 5.660) # example only
+x <- c( 1.309, 1.471, 1.490, 1.565, 1.611, 1.680)
+Nesterov.df <- data.frame(x,y)
+rm(x)
+rm(y)
+
+Nesterov.start <- function(indx) {
+  ...
+
+  start
+}
+#- Problem types will be determined from available functions / formulas
+#- ?? We could have problems with quotation marks WITHIN the code.
+#- lower = NA # if bounds are present, then we use bounded methods
+#- upper = NA # 
+
+#- function
+Nesterov.f <- function(x) { # ?? should remove -- this is a sumsquares problem
     n <- length(x)
     f <- (1 - x[1])^2/4
     for (i in 1:(n - 1)) {
@@ -7,7 +39,8 @@ fnNest<-function(x){
     f
 }
 
-grNest <- function(x) {
+#- gradient
+Nesterov.g<-function(x) {
     n <- length(x)
     g <- rep(0, n)
     g[1] <- (x[1] - 1)/2
@@ -18,3 +51,37 @@ grNest <- function(x) {
     }
     g
 }
+
+#- hessian
+Nesterov.h <- function(x) {
+#- THIS IS NOT COMPLETE??
+  stop("DanielWood.h IS NOT COMPLETE")
+  res<-DanielWood.res(x)
+  JJ<-DanielWood.jac(x)
+  pe$khess <- pe$khess + 1
+  H <- t(JJ) %*% JJ
+}
+
+
+#- residual
+Nesterov.res <- function(b) {
+return(res)
+}
+
+#- Jacobian
+# DanielWood - Jacobian
+Nesterov.jac <- function(b) {
+return(J)
+}
+
+#- nleq -- ?? section if appropriate
+
+
+#- test
+## put example calls of the function, possibly including calls to 
+# optimizations and nonlinear least squares etc.
+
+
+
+#- End DanielWood.prb   
+

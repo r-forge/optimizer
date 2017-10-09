d4665d970b6635792f6af9df46f15a07dcb52c43
nashjc (Prof ) <nashjc@uottawa.ca>
2017-10-09 03:28:13 +0000

    cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1635 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Roptanimation/inst/devfiles/polyexwball.R
+++
@@ -1,7 +0,0 @@
-library(optimrx)
-sall <-  opm(x0, polyobj, polygrad, method="ALL", lower=lb, upper=ub, 
-             control=list(trace=1, kkt=FALSE), penfactor=1e-5)
-sall <- summary(sall, order=value)
-print(sall)
-best1 <- coef(sall)[1,]
-polyarea(best1)
--- pkg/gradmin/R/lsnone.R
+++
@@ -1,9 +0,0 @@
-lsnone<-function(fn, fbest, xc, d, grv, w, ...) { 
-  # lsnone -- ALWAYS returns unit stop
-  # ?? count fevals?
-  rlout <- 1.0
-  w$nf <- w$nf+1
-  attr(rlout, "Fval")<- fn(xc + d)
-  attr(rlout, "FAIL")<- FALSE # can never fail
-  rlout
-} # end lsnone

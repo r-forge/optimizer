310252afdc1b95ef243db215bcf49a2e6fc43ff5
nashjc (Prof ) <nashjc@uottawa.ca>
2017-11-29 17:50:16 +0000

    move devnotes for optimr
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1655 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rvmmin/tests/BTbad.R
+++ pkg/Rvmmin/tests/BTbad.R
@@ -12,7 +12,7 @@ bt.g<-function(x){
 }
 
 n<-10
-xx <- rep(0,n)
+xx <- rep(0,n) # the start which is out of bounds
 lower <- rep(1,n)
 upper <- rep(3,n)
 bdmsk <- rep(1,n) # all free parameters
@@ -21,4 +21,12 @@ ansbt <- try( Rvmmin(xx, bt.f, bt.g, lower, upper, bdmsk,
 if (class(ansbt) == "try-error") {
    cat("Successful stop when out of bounds\n")
 } else { print(ansbt) }
+
+
+xx2 <- rep(2,n) # the start which is out of bounds
+ansbt2 <- try( Rvmmin(xx2, bt.f, bt.g, lower, upper, bdmsk, 
+         control=list(trace=1, keepinputpar = TRUE)) )
+if (class(ansbt2) == "try-error") {
+   cat("Successful stop when out of bounds\n")
+} else { print(ansbt2) }
 

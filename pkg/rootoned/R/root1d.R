af55d7a94083b4a5fc0d9da1bb3fdd3da6752ffe
nashjc (Prof ) <nashjc@uottawa.ca>
2018-08-20 21:32:43 +0000

    update rootoned
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1759 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rootoned/R/root1d.R
+++ pkg/rootoned/R/root1d.R
@@ -1,95 +1,109 @@
 root1d <- function(f, interval,
-             tol = .Machine$double.eps^0.5, maxiter = 1000, trace=FALSE, ...) {
-
-# trace <- FALSE
-#     uniroot(f, interval,
-#             lower = min(interval), upper = max(interval),
-#             f.lower = f(lower, ...), f.upper = f(upper, ...),
-#             tol = .Machine$double.eps^0.25, maxiter = 1000, ...)
-
-#var lbound, ubound: real;
-#                 var ifn: integer;
-#                     tol : real;
-#                 var noroot: boolean );
-
-#var
-# nbis: integer;
-# b, fb, flow, fup : real;
-# notcomp: boolean;
-
-
-if (trace) cat('alg18 == root1d -- root of a function of one variable\n')
-
-ubound <- interval[2]
-lbound <- interval[1]
-
-#  notcomp := FALSE; -- use try()
+                   tol = .Machine$double.eps^0.5, maxiter = 1000, trace=FALSE, ...) {
+  
+  # trace <- FALSE
+  #     uniroot(f, interval,
+  #             lower = min(interval), upper = max(interval),
+  #             f.lower = f(lower, ...), f.upper = f(upper, ...),
+  #             tol = .Machine$double.eps^0.25, maxiter = 1000, ...)
+  
+  #var lbound, ubound: real;
+  #                 var ifn: integer;
+  #                     tol : real;
+  #                 var noroot: boolean );
+  
+  #var
+  # nbis: integer;
+  # b, fb, flow, fup : real;
+  # notcomp: boolean;
+  
+  
+  if (trace) cat('alg18 == root1d -- root of a function of one variable\n')
+  
+  ubound <- interval[2]
+  lbound <- interval[1]
+  
+  #  notcomp := FALSE; -- use try()
   ifn <- 2
   nbis <- 5
   fup <- f(ubound,...)
-  fup0 <- fup # save it
-#  if notcomp then halt;
+  fup0<-fup # save it
+  #  if notcomp then halt;
   flow <- f(lbound,...)
-  flow0 <- flow # save it
-#  if notcomp then halt;
-#if (trace) cat('f(',lbound,')=',flow,'  f(',ubound,')=',fup,"\n")
-
-  if (fup * flow > 0) {
-     noroot  <-  TRUE
+  flow0<-flow # save it
+  #  if notcomp then halt;
+  #if (trace) cat('f(',lbound,')=',flow,'  f(',ubound,')=',fup,"\n")
+  
+  if (sign(fup)*sign(flow) > 0) {
+    noroot  <-  TRUE
   } else { noroot  <-  FALSE }
   op<-"start"
-  while ( (! noroot) && ((ubound-lbound)>tol) ) {
+  lastwidth <- (ubound - lbound)
+  while ( (! noroot) && ((ubound-lbound)>tol) && (ifn < maxiter)) {
     if (trace) cat("f(",lbound,")=",flow,"  f(",ubound,")=",fup,"  interval=",ubound-lbound,"  ",op,"\n")
-    if ( (nbis * ((ifn - 2) %/% nbis) == (ifn - 2)) ){
+    cat("Width reduced: ", (ubound - lbound  >= lastwidth - tol))
+    if ( (( (nbis * ((ifn - 2) %/% nbis) == (ifn - 2)) ) && (ifn > 2)) || (ubound - lbound  >= lastwidth - tol) ){
       op<-"Bisect"
-#if (trace) cat('Bisect  \n')
+      #if (trace) cat('Bisect  \n')
       b  <-  lbound + 0.5*(ubound - lbound)
     } else {
-#if (trace) cat('False position \n')
+      #if (trace) cat('False position \n')
       op<-"FalsePos"
-      if (identical(fup, flow)) {
-         if (trace) cat("b unchanged\n")
-         op <- "FP-nochange"
-      } else {
-         b  <-  (lbound*fup-ubound*flow)/(fup-flow)
-      }
+      ## 180820 JN: May give trouble if magnitudes wild
+      b  <-  (lbound*fup-ubound*flow)/(fup-flow)
     }
-
-## cat("b =",b,"  lbound=",lbound,"  ubound=",ubound,"\n") ## JN18
+    
+    #  cat("b =",b,"  lbound=",lbound,"  ubound=",ubound,"\n")
+    ## Edited by Oliver Dechant July 30th, 2018
+    ## 180820 Temporarily comment out. Do these stmts work for scalars?
+    #  b[is.na(b)] <- 0
+    #  lbound[is.na(lbound)] <- 0
+    ## END of edits
+    
+    #    if (b <= lbound) {
+    #      b  <-  lbound
+    #      ubound  <-  lbound # force convergence? We cannot compute an in-range value.
+    #    }
+    #    if (b >= ubound) { 
+    #      b  <-  ubound
+    #      lbound  <-  ubound # force convergence? We cannot compute an in-range value.
+    #    } # But we DO NOT break successfully -- not following code in CNM79
     if (b <= lbound) {
-      b  <-  lbound
-      ubound  <-  lbound
-    }
-    if (b >= ubound) { 
-      b  <-  ubound
-      lbound  <-  ubound
+      op <- "Fail-FP-low"
+      if (b <= lbound) {
+        ubound <- lbound
+      } else { lbound <- ubound} # close interval
+      cat("Fail-FP\n")
+      break
     }
     ifn  <-  ifn+1
-
     fb  <-  f(b, ...)
-#    if notcomp then halt;
-# if (trace) cat(ifn,' evalns: f(',b,')=',fb,"\n")
-# if (trace) cat('  width interval= ',(ubound-lbound),"\n")
+    #    if notcomp then halt; ## not implemented yet (would need try())
+    if (trace) cat(ifn,' evalns: f(',b,')=',fb,"\n")
+    if (trace) cat('  width interval= ',(ubound-lbound),"\n")
     if ( (ubound-lbound)>tol ){
-      if (fb*flow<0.0) {
+      if (sign(fb)*sign(flow) < 0.0) {
         fup  <-  fb
         ubound  <-  b
       } else {
         flow  <-  fb
         lbound  <-  b
       }
+      lastwidth <- ubound - lbound
     }
   } # end while
   if (noroot) { 
-	b<-NA # We did not find a root
-	fb<-NA
-  }
-if (trace)   cat('Converged to f(',b,')=',fb,"\n")
-if (trace)   cat('  Final interval width =',ubound-lbound,"\n")
+    b<-NA # We did not find a root
+    fb<-NA
+  } # end while loop
+  if (trace)   cat('Terminated at f(',b,')=',fb,"\n")
+  if (trace)   cat('  Final interval width =',ubound-lbound,"\n")
   if (! is.na(fb) && (abs(fb) > 0.5*max(abs(fup0), abs(flow0)))) {
-      msg<-"Final function magnitude > 0.5 * max(abs(f(lbound)), abs(f(ubound)))"
-      if (trace) cat(msg,"\n")
-      warning(msg)
+    msg<-"Final function magnitude > 0.5 * max(abs(f(lbound)), abs(f(ubound)))"
+    if (trace) cat(msg,"\n")
+    warning(msg)
   }
   res<-list(root=b, froot=fb, rtol=(ubound-lbound), fcount=ifn)
 }
+
+

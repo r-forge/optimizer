198582af715bac085ee6979737253f84fe59e366
dutangc (Mr ) <dutangc@gmail.com>
2019-12-20 19:52:29 +0000

    put version 0.99-2 (was never commited yet on CRAN)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1852 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/GNE/src/init.c
+++ pkg/GNE/src/init.c
@@ -14,20 +14,20 @@
 #include <R_ext/Rdynload.h>
 #include "SSR.h"
 
-//table of registration for call methods
-static const R_CallMethodDef callMethods[] = 
-{
-		{"dofunSSR", (DL_FUNC) &dofunSSR, 19},
-        {"dojacSSR", (DL_FUNC) &dojacSSR, 24},
-        {NULL, NULL, 0}
-};
-
-//table of registered routines
-void R_init_GNE(DllInfo *info)
-{
-        //register method accessed with .Call
-        R_registerRoutines(info, NULL, callMethods, NULL, NULL); 
-		
-	
-}
-
+ //table of registration for call methods
+ static const R_CallMethodDef callMethods[] = 
+   {
+   {"dofunSSR", (DL_FUNC) &dofunSSR, 19},
+   {"dojacSSR", (DL_FUNC) &dojacSSR, 24},
+   {NULL, NULL, 0}
+   };
+ 
+ //table of registered routines
+ void R_init_GNE(DllInfo *info)
+ {
+   //register method accessed with .Call
+   R_registerRoutines(info, NULL, callMethods, NULL, NULL); 
+   R_useDynamicSymbols(info, FALSE);
+   R_forceSymbols(info, TRUE);
+ }
+ 

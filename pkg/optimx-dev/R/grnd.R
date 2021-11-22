8c626405273db0002bfa0447d4e56578676aee79
nashjc (Prof ) <nashjc@uottawa.ca>
2021-11-22 15:31:35 +0000

    remove optimx-dev from repo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1886 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optimx-dev/R/grnd.R
+++
@@ -1,7 +0,0 @@
-############### grnd.R ####################
-grnd<-function(par, userfn, ...) { # using grad from numDeriv
-# with Richardson method
-   tryg<-grad(userfn, par, ...)
-}
-############### end grnd ####################
-

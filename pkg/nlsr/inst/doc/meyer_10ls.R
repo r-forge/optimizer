fa6cc017ecb97c636996a324fdd66573509c883d
nashjc (Prof ) <nashjc@uottawa.ca>
2022-06-16 18:46:43 +0000

    more tries
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1904 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nlsr/inst/doc/meyer_10ls.R
+++ pkg/nlsr/inst/doc/meyer_10ls.R
@@ -5,11 +5,15 @@ m <- 16
 t <- 45 + 5 * (1:m)
 df <- data.frame(t, y)
 modl <- y ~ x1 * exp(x2/(t + x3))
+modlp <- y ~ exp(x2/(t + x3))
 library(minpack.lm)
 library(nlsr)
 cat("nls:\n")
 anls<-nls(formula=modl, start=c(x1=1, x2=1, x3=1), data=df, trace=TRUE)
 summary(anls) # fails singular gradient
+cat("NOTE: modlp NOT modl\n")
+anlsp<-nls(formula=modlp, start=c(x2=1, x3=1), data=df, algorithm="plinear", trace=TRUE)
+summary(anlsp) # fails singular gradient
 anlxb<-nlxb(formula=modl, start=c(x1=1, x2=1, x3=1), data=df, trace=TRUE)
 summary(anlxb) # gets to min but slowly
 anlxb
@@ -18,4 +22,9 @@ summary(anlsLM) ## not near min
 library(nlsj)
 anlsj<-nlsj(formula=modl, start=c(x1=1, x2=1, x3=1), data=df, trace=TRUE)
 summary(anlsj) # NOT good
+library(nlsralt)
+anlxbx<-nlxbx(formula=modl, start=c(x1=1, x2=1, x3=1), data=df, trace=TRUE)
+summary(anlxbx) # gets to min but slowly (using unscaled)
+anlxbx
+
 

a6bc3ac63b9392e0a84a102f1aa47fd5138da5bc
nashjc (Prof ) <nashjc@uottawa.ca>
2022-06-16 18:21:02 +0000

    update meyer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimizer@1902 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nlsr/inst/doc/meyer_10ls.R
+++ pkg/nlsr/inst/doc/meyer_10ls.R
@@ -1,9 +1,9 @@
 # Meyer_10 function as a least squares problem
 y <- c(34780, 28610, 23650, 19630, 16370, 13720, 11540, 9744, 8261, 7030,
          6005, 5147, 4427, 3820, 3307, 2872)
+m <- 16
 t <- 45 + 5 * (1:m)
 df <- data.frame(t, y)
-m <- 16
 modl <- y ~ x1 * exp(x2/(t + x3))
 library(minpack.lm)
 library(nlsr)
